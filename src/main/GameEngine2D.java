640a05bade49ac8ee42547b45200d67d8eb75d1f
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-09-29 20:13:59 -0700

    Removed bin directory to hide it from git

--- src/main/GameEngine2D.java
+++ src/main/GameEngine2D.java
@@ -4,7 +4,7 @@ public class GameEngine2D {
 	public static void main(String[] args) {
 		System.setProperty("java.library.path", "libraries/natives/");
 		System.setProperty("org.lwjgl.librarypath", "libraries/natives/");
-		EventManager eventManager = new EventManager();		
+		EventManager eventManager = new EventManager();
 		new Thread(new GameManager(eventManager), "Game Manager").start();
 		eventManager.run();
 	}
