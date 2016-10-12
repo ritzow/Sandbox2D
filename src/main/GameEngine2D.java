7f2f6afd500d4580086d77e9994c39d5662e2055
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-10-12 09:54:30 -0700

    Refactored library locations

--- src/main/GameEngine2D.java
+++ src/main/GameEngine2D.java
@@ -2,9 +2,8 @@ package main;
 
 public class GameEngine2D {
 	public static void main(String[] args) {
-		System.setProperty("org.lwjgl.librarypath", "libraries/archives/lwjgl/natives");
-		System.setProperty("java.library.path", "libraries/archives/lwjgl/natives");
 		EventManager eventManager = new EventManager();
+		System.out.println(System.getProperty("user.dir"));
 		new Thread(new GameManager(eventManager), "Game Manager").start();
 		eventManager.run();
 	}
