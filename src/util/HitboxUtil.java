c26982964c0e33402e2c873af39727a6f393b95a
Solomon Ritzow <solduck@gmail.com>
2016-10-12 23:02:13 -0700

    Updated friction and collision system
    Still need to fix strange velocity/friction/player control bugs

--- src/util/HitboxUtil.java
+++ src/util/HitboxUtil.java
@@ -8,4 +8,8 @@ public class HitboxUtil {
 	public static boolean intersection(float x, float y, float width, float height, float x2, float y2, float width2, float height2) {
 		return (Math.abs(x - x2) * 2 < (width + width2)) && (Math.abs(y - y2) * 2 < (height + height2));
 	}
+	
+	public static float combineFriction(float friction1, float friction2) {
+		return (friction1 + friction2)/2;
+	}
 }
