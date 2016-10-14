bd2521b1e4f61135e82779c630df291c334b7ab3
Solomon Ritzow <solduck@gmail.com>
2016-10-13 21:06:36 -0700

    Renamed PlayerController to EntityController

--- src/input/controller/EntityController.java
+++ src/input/controller/EntityController.java
@@ -4,16 +4,15 @@ import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
 import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
 
 import input.Controls;
-import input.InputManager;
 import input.handler.KeyHandler;
 import main.Updatable;
 import world.World;
-import world.entity.GenericEntity;
+import world.entity.Entity;
 
-public final class PlayerController implements KeyHandler, Updatable {
+public final class EntityController implements KeyHandler, Updatable {
 	
 	private World world;
-	private GenericEntity player;
+	private Entity entity;
 	private float movementSpeed;
 	
 	private boolean upPressed;
@@ -21,34 +20,38 @@ public final class PlayerController implements KeyHandler, Updatable {
 	private boolean downPressed;
 	private boolean leftPressed;
 	
-	public PlayerController(InputManager manager, GenericEntity player, World world, float movementSpeed) {
+	public EntityController(Entity entity, World world, float movementSpeed) {
 		this.world = world;
-		this.player = player;
+		this.entity = entity;
 		this.movementSpeed = movementSpeed;
-		manager.getKeyHandlers().add(this);
 	}
 	
 	public void update() {
-		if(upPressed && (world.entityBelow(player) || world.blockBelow(player))) {
-			player.velocity().setY(movementSpeed * 1.5f); //TODO check above to make sure player doesn't jump when ceiling is present
+		
+		if(leftPressed && !world.blockLeft(entity)) {
+			//entity.velocity().addX(-0.003f);
+			entity.velocity().setAccelerationX(-0.05f);
+		}
+		
+		else {
+			//entity.velocity().setAccelerationX(0);
+		}
+		
+		if(rightPressed && !world.blockRight(entity)) {
+			//entity.velocity().addX(0.003f);
+			entity.velocity().setAccelerationX(0.05f);
+		}
+		
+		else {
+			//entity.velocity().setAccelerationX(0);
+		}
+		
+		if(upPressed && (world.blockBelow(entity) || world.entityBelow(entity))) {
+			entity.velocity().setAccelerationY(world.getGravity() * 20);
 		}
 		
 		if(downPressed) {
-			player.getHitbox().setHeight(0.75f);
-			player.getGraphics().scale().setY(0.75f);
-		} else {
-			player.getHitbox().setHeight(1);
-			player.getGraphics().scale().setY(1);
-		}
-		
-		if(rightPressed && !world.blockRight(player)) {
-			player.velocity().setX(movementSpeed);
-		} else if(leftPressed && !world.blockLeft(player)) {
-			player.velocity().setX(-movementSpeed);
-		}
-		
-		if (!leftPressed && !rightPressed) {
-			player.velocity().setX(0);
+			
 		}
 	}
 
