b57c7dd60efe2dc218b22b8df2ad5b56e6e83a52
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-10-15 23:08:59 -0700

    Updated GameManager
    Added ElementManager
    Added world background generation
    Added info for world generation time

--- src/main/GameManager.java
+++ src/main/GameManager.java
@@ -3,9 +3,12 @@ package main;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
 import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
 
-//import audio.AudioManager;
+import audio.AudioManager;
 import graphics.Background;
 import graphics.GraphicsManager;
+import graphics.ui.DynamicLocation;
+import graphics.ui.ElementManager;
+import graphics.ui.element.button.BlockSwitcherButton;
 import input.Controls;
 import input.EventManager;
 import input.controller.CameraController;
@@ -13,10 +16,11 @@ import input.controller.CursorController;
 import input.controller.EntityController;
 import input.handler.KeyHandler;
 import input.handler.WindowCloseHandler;
-import util.ResourceManager;
+import util.ModelManager;
 import util.Synchronizer;
 import world.World;
 import world.WorldManager;
+import world.block.Block;
 import world.block.DirtBlock;
 import world.block.GrassBlock;
 import world.block.RedBlock;
@@ -25,7 +29,7 @@ import world.entity.GenericEntity;
 public final class GameManager implements Runnable, WindowCloseHandler, KeyHandler {
 	private EventManager eventManager;
 	private GraphicsManager graphicsManager;
-	//private AudioManager audioManager;
+	private AudioManager audioManager;
 	private WorldManager worldManager;
 	private ClientUpdateManager clientUpdateManager;
 	public static CameraController cameraController;
@@ -40,14 +44,17 @@ public final class GameManager implements Runnable, WindowCloseHandler, KeyHandl
 		eventManager.getDisplay().getInputManager().getKeyHandlers().add(this);
 		
 		new Thread(graphicsManager = new GraphicsManager(eventManager.getDisplay()), "Graphics Manager").start();
-		//new Thread(audioManager = new AudioManager(), "Audio Manager").start();
+		new Thread(audioManager = new AudioManager(), "Audio Manager").start();
 		
 		Synchronizer.waitForSetup(graphicsManager);
 		
-		World world = new World(200, 200, 0.015f);
-		for(int i = 0; i < world.getBlocks().getHeight()/2; i++) {
+		System.out.println("Creating world...");
+		long time = System.currentTimeMillis();
+		World world = new World(2000, 2000, 0.015f);
+		for(int i = 0; i < world.getBlocks().getHeight(); i++) {
 			for(int j = 0; j < world.getBlocks().getWidth(); j++) {
-				if(i == world.getBlocks().getHeight()/2 - 1) {
+				world.getBackground().set(j, i, new DirtBlock());
+				if(i == world.getBlocks().getHeight() - 1) {
 					world.getBlocks().set(j, i, new GrassBlock());
 				} else if(Math.random() < 0.005f) {
 					world.getBlocks().set(j, i, new RedBlock());
@@ -56,12 +63,13 @@ public final class GameManager implements Runnable, WindowCloseHandler, KeyHandl
 				}
 			}
 		}
+		System.out.println("World creation took " + ((System.currentTimeMillis() - time)/1000.0f) + " seconds");
 		
-		GenericEntity player = new GenericEntity(ResourceManager.GREEN_FACE);
+		GenericEntity player = new GenericEntity(ModelManager.GREEN_FACE);
 		player.getHitbox().setPriority(0.1f);
-		player.position().setX(world.getBlocks().getWidth()/2);
-		player.position().setY(world.getBlocks().getHeight());
-		player.getHitbox().setFriction(0.0f);
+		player.setPositionX(world.getBlocks().getWidth()/2);
+		player.setPositionY(world.getBlocks().getHeight());
+		player.getHitbox().setFriction(0.02f);
 		world.getEntities().add(player);
 		
 		EntityController playerController = new EntityController(player, world, 0.2f);
@@ -69,15 +77,23 @@ public final class GameManager implements Runnable, WindowCloseHandler, KeyHandl
 		CursorController cursorController = new CursorController(eventManager.getDisplay().getInputManager(), player, world, graphicsManager.getRenderer().getCamera(), 200);
 		cameraController = new CameraController(eventManager.getDisplay().getInputManager(), graphicsManager.getRenderer().getCamera(), player, 0.005f);
 		
+		ElementManager manager = new ElementManager();
+		eventManager.getDisplay().getInputManager().getCursorPosHandlers().add(manager);
+		eventManager.getDisplay().getInputManager().getMouseButtonHandlers().add(manager);
+		eventManager.getDisplay().getInputManager().getFramebufferSizeHandlers().add(manager);
+		manager.put(new BlockSwitcherButton(new Block[] {new DirtBlock(), new RedBlock(), new GrassBlock()}, cursorController), new DynamicLocation(-1f, -1f, 0.25f, 0.25f));
+		
 		new Thread(worldManager = new WorldManager(world), "World Manager " + world.hashCode()).start();
 		new Thread(clientUpdateManager = new ClientUpdateManager(), "Client Updater").start();
 		
 		clientUpdateManager.getUpdatables().add(playerController);
 		clientUpdateManager.getUpdatables().add(cursorController);
 		clientUpdateManager.getUpdatables().add(cameraController);
+		clientUpdateManager.getUpdatables().add(manager);
 		
-		graphicsManager.getRenderables().add(new Background(ResourceManager.CLOUDS_BACKGROUND));
+		graphicsManager.getRenderables().add(new Background(ModelManager.CLOUDS_BACKGROUND));
 		graphicsManager.getRenderables().add(world);
+		graphicsManager.getRenderables().add(manager);
 		
 		synchronized(eventManager) {
 			eventManager.setReadyToDisplay();
@@ -87,7 +103,7 @@ public final class GameManager implements Runnable, WindowCloseHandler, KeyHandl
 	
 	private void exit() {
 		clientUpdateManager.exit();
-		//Synchronizer.waitForExit(audioManager);
+		Synchronizer.waitForExit(audioManager);
 		Synchronizer.waitForExit(worldManager);
 		Synchronizer.waitForExit(graphicsManager);
 		eventManager.exit();
