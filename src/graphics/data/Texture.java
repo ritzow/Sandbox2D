d0a50a81955ccda245f8cdaf5d246b2f09dde995
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-10-19 19:47:28 -0700

    Miscellaneous changes

--- src/graphics/data/Texture.java
+++ src/graphics/data/Texture.java
@@ -1,7 +1,6 @@
 package graphics.data;
 
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL12.*;
 import static org.lwjgl.opengl.GL13.*;
 import static org.lwjgl.stb.STBImage.*;
 import org.lwjgl.BufferUtils;
@@ -30,8 +29,8 @@ public class Texture extends Buffer {
 		bind();
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 		unbind();
 	}
 	
