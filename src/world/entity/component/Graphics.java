e38ad986f727d79f40a353494024c75b8530baa0
Solomon Ritzow <solduck@gmail.com>
2016-10-23 23:30:06 -0700

    Refactored Graphics class

--- src/world/entity/component/Graphics.java
+++ src/world/entity/component/Graphics.java
@@ -1,46 +1,83 @@
 package world.entity.component;
 
 import graphics.Model;
+import graphics.Renderer;
 import java.io.Serializable;
 
 public class Graphics implements Serializable {
 	private static final long serialVersionUID = 4497437671840075738L;
 	
 	protected Model model;
-	protected Scale scale;
-	protected Rotation rotation;
-	
 	protected float opacity;
-
+	protected float scaleX;
+	protected float scaleY;
+	protected float rotation;
+	protected float rotationVelocity;
+	
 	public Graphics(Model model) {
+		this(model, 1.0f, 1.0f, 1.0f, 0.0f);
+	}
+	
+	public Graphics(Model model, float opacity, float scaleX, float scaleY, float rotation) {
 		this.model = model;
-		this.scale = new Scale(1, 1);
-		this.rotation = new Rotation(0);
-		this.opacity = 1;
+		this.opacity = opacity;
+		this.scaleX = scaleX;
+		this.scaleY = scaleY;
+		this.rotation = rotation;
 	}
 	
-	public Scale getScale() {
-		return scale;
+	public void render(Renderer renderer, float x, float y) {
+		renderer.loadOpacity(opacity);
+		renderer.loadTransformationMatrix(x, y, scaleX, scaleY, rotation);
+		model.render();
 	}
-	
-	public Rotation getRotation() {
-		return rotation;
-	}
-	
-	public Model getModel() {
+
+	public final Model getModel() {
 		return model;
 	}
-	
-	public void setModel(Model model) {
-		this.model = model;
-	}
-	
-	public float getOpacity() {
+
+	public final float getOpacity() {
 		return opacity;
 	}
-	
-	public void setOpacity(float opacity) {
+
+	public final float getScaleX() {
+		return scaleX;
+	}
+
+	public final float getScaleY() {
+		return scaleY;
+	}
+
+	public final float getRotation() {
+		return rotation;
+	}
+
+	public final float getRotationVelocity() {
+		return rotationVelocity;
+	}
+
+	public final void setModel(Model model) {
+		this.model = model;
+	}
+
+	public final void setOpacity(float opacity) {
 		this.opacity = opacity;
+	}
+
+	public final void setScaleX(float scaleX) {
+		this.scaleX = scaleX;
+	}
+
+	public final void setScaleY(float scaleY) {
+		this.scaleY = scaleY;
+	}
+
+	public final void setRotation(float rotation) {
+		this.rotation = rotation;
+	}
+
+	public final void setRotationVelocity(float rotationVelocity) {
+		this.rotationVelocity = rotationVelocity;
 	}
 	
 }
