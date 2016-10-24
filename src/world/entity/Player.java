585c072b2c7e314e9ec7746c91cb38151d80acd7
Solomon Ritzow <solduck@gmail.com>
2016-10-23 23:30:43 -0700

    Made basic player character

--- src/world/entity/Player.java
+++ src/world/entity/Player.java
@@ -1,6 +1,8 @@
 package world.entity;
 
 import graphics.Renderer;
+import resource.Models;
+import world.entity.component.Graphics;
 import world.item.Item;
 
 public class Player extends LivingEntity {
@@ -9,14 +11,29 @@ public class Player extends LivingEntity {
 	
 	protected final Item[] inventory;
 	
-	public Player(int inventorySize) {
+	protected Graphics head;
+	protected Graphics body;
+	
+	public Player() {
 		super(100);
-		inventory = new Item[inventorySize];
+		this.mass = 1.0f;
+		this.friction = 0.02f;
+		this.width = 1.0f;
+		this.height = 2.0f;
+		this.head = new Graphics(Models.GREEN_FACE, 1.0f, 1.0f, 1.0f, 0.0f);
+		this.body = new Graphics(Models.BLUE_SQUARE, 1.0f, 1.0f, 1.0f, 0.0f);
+		this.inventory = new Item[20];
+	}
+	
+	public void update(float milliseconds) {
+		super.update(milliseconds);
+		body.setRotation(body.getRotation() + velocityX);
 	}
 
 	@Override
 	public void render(Renderer renderer) {
-		
+		head.render(renderer, positionX, positionY + 0.5f);
+		body.render(renderer, positionX, positionY - 0.5f);
 	}
 
 }
