17426f1b9ce820848c843f4389467ffeeb9bca5d
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-10-26 23:32:54 -0700

    Improved autojump and block detection

--- src/input/controller/EntityController.java
+++ src/input/controller/EntityController.java
@@ -2,6 +2,7 @@ package input.controller;
 
 import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
 import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static util.HitboxUtil.intersection;
 
 import input.Controls;
 import input.InputManager;
@@ -35,23 +36,23 @@ public final class EntityController extends Controller implements KeyHandler, Up
 	
 	@Override
 	public void update() {
-
+		
 		if(left) {
-			if(!world.blockLeft(entity)) {
+			if(!blockLeft()) {
 				entity.setVelocityX(-movementSpeed);
 			}
 			
-			else if(entity.getVelocityY() <= 0 && !world.getForeground().isBlock(entity.getPositionX() - 1, entity.getPositionY() + 1) && canJump()) {
+			else if(!blockLeft(entity.getPositionX(), entity.getPositionY() + getJumpHeight(world.getGravity()), entity.getWidth(), entity.getHeight()) && canJump()) {
 				entity.setVelocityY(movementSpeed);
 			}
 		}
 		
 		if(right) {
-			if(!world.blockRight(entity)) {
+			if(!blockRight()) {
 				entity.setVelocityX(movementSpeed);
 			}
 			
-			else if(entity.getVelocityY() <= 0 && !world.getForeground().isBlock(entity.getPositionX() + 1, entity.getPositionY() + 1) && canJump()) {
+			else if(!blockRight(entity.getPositionX(), entity.getPositionY() + getJumpHeight(world.getGravity()), entity.getWidth(), entity.getHeight()) && canJump()) {
 				entity.setVelocityY(movementSpeed);
 			}
 		}
@@ -69,8 +70,63 @@ public final class EntityController extends Controller implements KeyHandler, Up
 		}
 	}
 	
+	private float getJumpHeight(float gravity) {
+		return (0 - (float)Math.pow(movementSpeed, 2))/(2 * -gravity);
+	}
+	
 	private boolean canJump() {
-		return entity.getVelocityY() <= 0 && (world.entityBelow(entity) || world.blockBelow(entity));
+		return entity.getVelocityY() <= 0 && (entityBelow() || blockBelow());
+	}
+	
+	private boolean blockBelow() {
+		return blockBelow(entity.getPositionX(), entity.getPositionY(), entity.getWidth(), entity.getHeight());
+	}
+	
+	private boolean blockBelow(float x, float y, float width, float height) {
+		for(float h = x - width/2; h <= x + width/2; h++) {	
+			if(world.getForeground().isBlock(h + (h < x ? +0.05f : -0.05f), y - height/2 - 0.05f)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private boolean blockLeft() {
+		return blockLeft(entity.getPositionX(), entity.getPositionY(), entity.getWidth(), entity.getHeight());
+	}
+	
+	private boolean blockLeft(float x, float y, float width, float height) {
+		for(float v = y - height/2; v <= y + height/2; v++) {
+			if(world.getForeground().isBlock(x - width/2 - 0.05f, v + (v < y ? +0.05f : -0.05f))) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private boolean blockRight() {
+		return blockRight(entity.getPositionX(), entity.getPositionY(), entity.getWidth(), entity.getHeight());
+	}
+	
+	private boolean blockRight(float x, float y, float width, float height) {
+		for(float v = y - height/2; v <= y + height/2; v++) {
+			if(world.getForeground().isBlock(x + width/2 + 0.05f, v + (v < y ? +0.05f : -0.05f))) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private boolean entityBelow() {
+		for(int i = 0; i < world.getEntities().size(); i++) {
+			Entity o = world.getEntities().get(i);
+			if(o != null && entity != o && o.getDoEntityCollisionResolution() && 
+					intersection(entity.getPositionX(), entity.getPositionY() - entity.getHeight()/2, entity.getWidth() - 0.01f, 0.1f, 
+							o.getPositionX(), o.getPositionY(), o.getWidth(), o.getHeight())) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	@Override
