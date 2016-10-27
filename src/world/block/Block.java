098c612296289508298e3abc3b8887da0c65ebbc
Solomon Ritzow <solduck@gmail.com>
2016-10-26 17:57:16 -0700

    Created ParticleEntity class
    The ParticleEntity class can be used for some particle effects, but is
    inefficient and not the final iteration of the particle system. I may
    add a ParticleEmitter later

--- src/world/block/Block.java
+++ src/world/block/Block.java
@@ -1,12 +1,14 @@
 package world.block;
 
+import java.io.Serializable;
+
 import audio.AudioSystem;
 import graphics.Model;
-import java.io.Serializable;
 import resource.Sounds;
 import world.World;
-import world.entity.GraphicsEntity;
 import world.entity.ItemEntity;
+import world.entity.ParticleEntity;
+import world.entity.component.Graphics;
 import world.item.BlockItem;
 
 public abstract class Block implements Serializable {
@@ -34,17 +36,9 @@ public abstract class Block implements Serializable {
 	public void onPlace(World world, float x, float y) {
 		AudioSystem.playSound(Sounds.BLOCK_PLACE, x, y, 0, 0, 1, 1);
 		for(int i = 0; i < 5; i++) {
-			GraphicsEntity particle = new GraphicsEntity(getModel());
-			particle.setDoBlockCollision(false);
-			particle.setDoEntityCollision(false);
-			particle.getGraphics().setScaleX(0.2f);
-			particle.getGraphics().setScaleY(0.2f);
-			particle.setPositionX(x);
-			particle.setPositionY(y);
-			particle.setMass(0);
-			particle.getGraphics().setRotationVelocity((float)Math.random() - 0.5f);
-			particle.setVelocityX(-0.2f + ((float)Math.random() * (0.4f))); //min + ((float)Math.random() * (max - min))
-			particle.setVelocityY((float)Math.random() * (0.35f));
+			ParticleEntity particle = new ParticleEntity(new Graphics(getModel(), 1.0f, 0.3f, 0.3f, 0), x, y, 500, (float)Math.random() * 0.4f - 0.2f, true);
+			particle.setVelocityX((float)Math.random() * 0.4f - 0.2f);
+			particle.setVelocityY((float)Math.random() * 0.35f);
 			world.getEntities().add(particle);
 		}
 	}
---
+++ src/world/entity/ParticleEntity.java
@@ -0,0 +1,63 @@
+package world.entity;
+
+import graphics.Renderer;
+import world.World;
+import world.entity.component.Graphics;
+
+public class ParticleEntity extends Entity {
+	private static final long serialVersionUID = 7392722072442474321L;
+	
+	protected Graphics graphics;
+	protected long birthtime;
+	protected long lifetime;
+	protected float rotationSpeed;
+	protected boolean fade;
+
+	public ParticleEntity(Graphics graphics, float posX, float posY, long lifetime, float rotationSpeed, boolean fade) {
+		this.graphics = graphics;
+		this.positionX = posX;
+		this.positionY = posY;
+		this.rotationSpeed = rotationSpeed;
+		this.lifetime = lifetime;
+		this.fade = fade;
+		this.doBlockCollisionResolution = false;
+		this.doEntityCollisionResolution = false;
+		this.birthtime = System.currentTimeMillis();
+	}
+	
+	public void startLife() {
+		
+	}
+	
+	public void update(float milliseconds) {
+		float remaining = this.getLifetimeRemaining();
+		
+		if(remaining <= 0) {
+			this.shouldDelete = true;
+		}
+		
+		else {
+			super.update(milliseconds);
+			graphics.setRotation(graphics.getRotation() + rotationSpeed);
+			
+			if(fade) {
+				float opacity = remaining/lifetime;
+				graphics.setOpacity(opacity);
+			}
+		}
+	}
+
+	@Override
+	public void render(Renderer renderer) {
+		graphics.render(renderer, positionX, positionY);
+	}
+
+	@Override
+	public void onCollision(World world, Entity e) {
+		
+	}
+	
+	protected float getLifetimeRemaining() {
+		return lifetime - (System.currentTimeMillis() - birthtime);
+	}
+}
