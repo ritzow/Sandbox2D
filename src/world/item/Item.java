dd0ccf37d3798c87b5c6726cca2875ba783288be
Solomon Ritzow <solduck@gmail.com>
2016-10-27 23:21:08 -0700

    Changed Item class to only require a getModel method

--- src/world/item/Item.java
+++ src/world/item/Item.java
@@ -3,23 +3,5 @@ package world.item;
 import graphics.Model;
 
 public abstract class Item {
-	protected Model model;
-	protected float mass;
-	
-	public final Model getModel() {
-		return model;
-	}
-	
-	public final float getMass() {
-		return mass;
-	}
-	
-	public final void setModel(Model model) {
-		this.model = model;
-	}
-	
-	public final void setMass(float mass) {
-		this.mass = mass;
-	}
-	
+	public abstract Model getModel();
 }
