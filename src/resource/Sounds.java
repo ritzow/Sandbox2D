3461661f2171e5c824c79d11c9029b6527203747
Solomon Ritzow <solduck@gmail.com>
2016-10-27 23:19:31 -0700

    Added temporary item pickup sound

--- src/resource/Sounds.java
+++ src/resource/Sounds.java
@@ -11,11 +11,13 @@ public class Sounds {
 	public static int BLOCK_BREAK;
 	public static int BLOCK_PLACE;
 	public static int BLOOP;
+	public static int ITEM_PICKUP;
 	
 	public static void loadAll(File directory) throws IOException {
 		Sounds.BLOCK_BREAK = loadAudio(new File(directory.getPath(), "blockBreak.wav"));
 		Sounds.BLOCK_PLACE = loadAudio(new File(directory.getPath(), "blockPlace.wav"));
 		Sounds.BLOOP = loadAudio(new File(directory.getPath(), "bloop.wav"));
+		Sounds.ITEM_PICKUP = loadAudio(new File(directory.getPath(), "itemPickup.wav"));
 	}
 	
 	public static int loadAudio(File file) throws IOException {
