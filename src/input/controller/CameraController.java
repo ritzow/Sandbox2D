3cca0cf4bb08cbab35b34941747d51284a4fec3c
Solomon Ritzow <solduck@gmail.com>
2016-10-30 22:47:22 -0700

    Reworked CameraController system, still needs changes

--- src/input/controller/CameraController.java
+++ src/input/controller/CameraController.java
@@ -1,88 +1,25 @@
 package input.controller;
 
-import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_MIDDLE;
-import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
-import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
-
-import audio.AudioSystem;
-import graphics.Camera;
-import input.Controls;
 import input.InputManager;
 import input.handler.KeyHandler;
 import input.handler.MouseButtonHandler;
 import input.handler.ScrollHandler;
 import util.Updatable;
-import world.entity.Entity;
 
-public class CameraController extends Controller implements KeyHandler, ScrollHandler, MouseButtonHandler, Updatable {
-	
-	protected Camera camera;
-	protected float zoomSpeed;
-	protected float soundFalloff;
-	
-	protected Entity target;
-	
-	public CameraController(Camera camera, Entity target, float zoomSpeed) {
-		this.camera = camera;
-		this.zoomSpeed = zoomSpeed;
-		this.target = target;
-		this.soundFalloff = 1.5f;
-	}
-	
-	public void update() {
-		camera.setX(target.getPositionX());
-		camera.setY(target.getPositionY());
-		camera.update();
-		AudioSystem.setListenerProperties(camera.getX(), camera.getY(), 
-				-(camera.getMaxZoom() - camera.getZoom()) * soundFalloff, target.getVelocityX(), target.getVelocityY(), 0, 0);
-	}
-	
+public abstract class CameraController extends Controller implements KeyHandler, ScrollHandler, MouseButtonHandler, Updatable {
+
 	@Override
-	public void link(InputManager input) {
-		input.getKeyHandlers().add(this);
-		input.getScrollHandlers().add(this);
-		input.getMouseButtonHandlers().add(this);
+	public void link(InputManager manager) {
+		manager.getKeyHandlers().add(this);
+		manager.getScrollHandlers().add(this);
+		manager.getMouseButtonHandlers().add(this);
 	}
 
 	@Override
-	public void unlink(InputManager input) {
-		input.getKeyHandlers().remove(this);
-		input.getScrollHandlers().remove(this);
-		input.getMouseButtonHandlers().remove(this);
+	public void unlink(InputManager manager) {
+		manager.getKeyHandlers().remove(this);
+		manager.getScrollHandlers().remove(this);
+		manager.getMouseButtonHandlers().remove(this);
 	}
 
-	@Override
-	public void mouseScroll(double xoffset, double yoffset) {
-		camera.setZoom(camera.getZoom() + camera.getZoom() * (float)yoffset * 0.2f);
-	}
-	
-	@Override
-	public void mouseButton(int button, int action, int mods) {
-		if(button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_PRESS) {
-			camera.resetZoom();
-		}
-	}
-
-	@Override
-	public void keyboardButton(int key, int scancode, int action, int mods) {
-		if(key == Controls.KEYBIND_INCREASEZOOM) {
-			if(action == GLFW_PRESS) {
-				camera.setVelocityZ(camera.getVelocityZ() + zoomSpeed);
-			}
-			
-			else if(action == GLFW_RELEASE) {
-				camera.setVelocityZ(0);
-			}
-		}
-		
-		else if(key == Controls.KEYBIND_DECREASEZOOM) {
-			if(action == GLFW_PRESS) {
-				camera.setVelocityZ(camera.getVelocityZ() - zoomSpeed);
-			}
-			
-			else if(action == GLFW_RELEASE) {
-				camera.setVelocityZ(0);
-			}
-		}
-	}
 }
