09917ea66ad45b6a09d2e1b45cef4cf2d6bff04c
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-10-30 22:49:11 -0700

    Started fiddling with networking

--- src/network/Client.java
+++ src/network/Client.java
@@ -1,5 +1,67 @@
 package network;
+import java.io.*;
+import java.net.*;
 
 public class Client {
-
+	
+	Socket client;
+	
+	public Client(InetAddress address, int port) throws IOException {
+		this.client = new Socket(address, port);
+	}
+	
+	public boolean sendMessage(String message) {
+		try {
+			client.getOutputStream().write((message + '\u0000').getBytes());
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+	
+	public boolean sendObject(Object o) {
+		try {
+			new ObjectOutputStream(client.getOutputStream()).writeObject(o);
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+	
+	public String getMessage() {
+		StringBuilder s = new StringBuilder();
+		char buffer;
+		
+		try {
+			while((buffer = (char)client.getInputStream().read()) != '\u0000') {
+				s.append(buffer);
+			}
+		} 
+		
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return s.toString();
+	}
+	
+	public Object getObject() {
+		try {
+			return (new ObjectInputStream(client.getInputStream())).readObject();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public void close() {
+		try {
+			client.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
