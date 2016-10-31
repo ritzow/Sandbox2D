ab8610f5c927ab82941beaf35afc842dfc2b2560
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-10-30 22:46:48 -0700

    Removed all unneccessary information from Camera
    All movement related aspects of a Camera should be handled in a
    CameraController

--- src/graphics/Camera.java
+++ src/graphics/Camera.java
@@ -1,74 +1,37 @@
 package graphics;
 
 public class Camera {
-	protected float posX;
-	protected float posY;
+	protected float positionX;
+	protected float positionY;
 	protected float zoom;
-	protected float minZoom;
-	protected float maxZoom;
-	protected float velocityZ;
 	
-	public Camera(float posX, float posY, float minZoom, float maxZoom) {
-		this.posX = posX;
-		this.posY = posY;
-		this.minZoom = minZoom;
-		this.maxZoom = maxZoom;
-		resetZoom();
-	}
-	
-	public void resetZoom() {
-		this.zoom = minZoom + maxZoom * 0.1f;
-	}
-	
-	public void update() {
-		setZoom(zoom + zoom * velocityZ);
-	}
-	
-	public final float getMinZoom() {
-		return minZoom;
+	public Camera(float positionX, float positionY, float zoom) {
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.zoom = zoom;
 	}
 
-	public final float getMaxZoom() {
-		return maxZoom;
+	public final float getPositionX() {
+		return positionX;
 	}
-	
-	public final float getVelocityZ() {
-		return velocityZ;
+
+	public final float getPositionY() {
+		return positionY;
 	}
-	
+
 	public final float getZoom() {
 		return zoom;
 	}
-	
-	public final void setVelocityZ(float velocityZ) {
-		this.velocityZ = velocityZ;
-	}
-	
-	public final void setMinZoom(float minZoom) {
-		this.minZoom = minZoom;
+
+	public final void setPositionX(float positionX) {
+		this.positionX = positionX;
 	}
 
-	public final void setMaxZoom(float maxZoom) {
-		this.maxZoom = maxZoom;
+	public final void setPositionY(float positionY) {
+		this.positionY = positionY;
 	}
-	
+
 	public final void setZoom(float zoom) {
-		this.zoom = Math.max(Math.min(zoom, maxZoom), minZoom);
-	}
-
-	public float getX() {
-		return posX;
-	}
-
-	public float getY() {
-		return posY;
-	}
-
-	public void setX(float x) {
-		posX = x;
-	}
-
-	public void setY(float y) {
-		posY = y;
+		this.zoom = zoom;
 	}
 }
