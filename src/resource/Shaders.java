31bf6eea97f7a8f068b11dcac857d812fe9d6e44
Solomon Ritzow <solduck@gmail.com>
2016-10-30 22:49:29 -0700

    Created Shaders class, currently not used

---
+++ src/resource/Shaders.java
@@ -0,0 +1,45 @@
+package resource;
+
+import static org.lwjgl.opengl.GL20.glCompileShader;
+import static org.lwjgl.opengl.GL20.glCreateShader;
+import static org.lwjgl.opengl.GL20.glShaderSource;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import org.lwjgl.BufferUtils;
+import static org.lwjgl.opengl.GL20.*;
+
+public class Shaders {
+	public static int VERTEX_SHADER;
+	public static int FRAGMENT_SHADER;
+	
+	public static void loadAll(File directory) throws IOException {
+		Shaders.VERTEX_SHADER = loadShader(new File(directory, "vertexShader"), GL_VERTEX_SHADER);
+		Shaders.FRAGMENT_SHADER = loadShader(new File(directory, "fragmentShader"), GL_FRAGMENT_SHADER);
+	}
+	
+	public static void deleteAll() {
+		glDeleteShader(VERTEX_SHADER);
+		glDeleteShader(FRAGMENT_SHADER);
+	}
+	
+	public static int loadShader(File file, int OpenGLShaderType) throws IOException {
+		FileInputStream reader = new FileInputStream(file);
+		
+		ByteBuffer data = BufferUtils.createByteBuffer((int)file.length());
+		
+		while(data.hasRemaining()) {
+			data.put((byte)reader.read());
+		}
+		
+		 
+		reader.close();
+		int shader = glCreateShader(OpenGLShaderType);
+		glShaderSource(shader, data.asCharBuffer());
+		glCompileShader(shader);
+		
+		return shader;
+	}
+}
