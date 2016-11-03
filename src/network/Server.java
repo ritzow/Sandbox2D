3467c7490b6e01038e84d7d9d68a1a93c18d1965
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-11-02 23:00:30 -0700

    Created some experimental server/client/UDP code

--- src/network/Server.java
+++ src/network/Server.java
@@ -3,31 +3,99 @@ package network;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import util.Exitable;
 import util.Installable;
-import util.Synchronizer;
 
 public class Server implements Runnable, Installable, Exitable {
 
 	private volatile boolean exit;
 	private volatile boolean setupComplete;
 	private volatile boolean finished;
-	
-	private ConnectionListener connectionListener;
-	private ServerSocket socket;
+	private int port;
 	
 	private ArrayList<Socket> connections;
+	private ArrayList<MessageHandler> listeners;
+	private volatile ServerSocket connectionListener;
 	
 	public Server(int port) {
+		this.port = port;
 		connections = new ArrayList<Socket>();
-		try {
-			this.socket = new ServerSocket(port);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		listeners = new ArrayList<MessageHandler>();
 	}
 
+	@Override
+	public void run() {
+		new Thread("Server Connection Listener") {
+			public void run() {
+				try {
+					connectionListener = new ServerSocket(port);
+
+					try {
+						while(!exit) {
+							Socket newConnection = connectionListener.accept();
+							
+							if(newConnection != null) {
+								synchronized(connections) {
+									connections.add(newConnection);
+									MessageHandler listener = new MessageHandler(newConnection);
+									new Thread(listener, "Message Listener " + listener.hashCode()).start();
+									listeners.add(listener);
+								}
+							}
+						}
+					} catch(SocketException e) {
+						//server socket is closed while waiting for an incoming connection
+					}
+					
+					connectionListener.close();
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();
+		
+		new Thread("Server Input Listener") {
+			public void run() {
+				while(!exit) {
+					for(int i = 0; i < connections.size(); i++) {
+						//TODO put all of this stuff on a separate thread per client (REMOVE THIS THREAD)
+						
+					}
+				}
+			}
+		}.start();
+		
+		synchronized(this) {
+			setupComplete = true;
+			this.notifyAll();
+		}
+		
+		while(!exit) {
+			for(int i = 0; i < connections.size(); i++) {
+				synchronized(connections) {
+					//TODO send data to clients
+				}
+			}
+		}
+		
+		try {
+			for(int i = 0; i < connections.size(); i++) {
+				connections.get(i).close();
+			}
+			connectionListener.close();
+		} catch (IOException e) {
+			
+		}
+		
+		synchronized(this) {
+			this.finished = true;
+			this.notifyAll();
+		}
+	}
+	
 	@Override
 	public synchronized void exit() {
 		this.exit = true;
@@ -42,76 +110,4 @@ public class Server implements Runnable, Installable, Exitable {
 	public synchronized boolean isSetupComplete() {
 		return setupComplete;
 	}
-
-	@Override
-	public void run() {
-		
-		new Thread(connectionListener = new ConnectionListener(socket, connections)).start();
-		
-		synchronized(this) {
-			setupComplete = true;
-			this.notifyAll();
-		}
-		
-		while(!exit) {
-			for(int i = 0; i < connections.size(); i++) {
-				//TODO send data to connections?
-
-			}
-			try {
-				Thread.sleep(1);
-			} catch (InterruptedException e) {
-
-			}
-		}
-		
-		Synchronizer.waitForExit(connectionListener);
-		
-		synchronized(this) {
-			this.finished = true;
-			this.notifyAll();
-		}
-	}
-	
-	private class ConnectionListener implements Runnable, Exitable {
-		
-		private ArrayList<Socket> connections;
-		private boolean exit;
-		private boolean finished;
-		
-		private ServerSocket server;
-		
-		public ConnectionListener(ServerSocket server, ArrayList<Socket> connections) {
-			this.connections = connections;
-			this.server = server;
-		}
-
-		@Override
-		public void run() {
-			while(!exit) {
-				try {
-					connections.add(server.accept());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			
-			synchronized(this) {
-				finished = true;
-				this.notifyAll();
-			}
-		}
-
-		@Override
-		public synchronized void exit() {
-			this.exit = true;
-		}
-
-		@Override
-		public synchronized boolean isFinished() {
-			return finished;
-		}
-		
-	}
-
 }
