5932e03bfc0c7625e78ae774ee840edf3b161606
Solomon Ritzow <solduck@gmail.com>
2016-11-05 19:21:30 -0700

    Added PRINT_MEMORY_USAGE flag to GameEngine2D
    Signals to GameManager to print the memory usage of the program every
    second.

--- src/main/GameEngine2D.java
+++ src/main/GameEngine2D.java
@@ -3,7 +3,10 @@ package main;
 import input.EventManager;
 
 public class GameEngine2D {
-	public static void main(String[] args) {
+	
+	public static final boolean PRINT_MEMORY_USAGE = false;
+	
+	public static void main(String[] args) throws InterruptedException {
 		EventManager eventManager = new EventManager();
 		System.out.println("Working directory: " + System.getProperty("user.dir"));
 		new Thread(new GameManager(eventManager), "Game Manager").start();
