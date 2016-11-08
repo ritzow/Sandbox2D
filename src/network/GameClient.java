6aba7adf5a98f3572c56057c3a10f87a4ed48446
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-11-07 22:11:37 -0800

    Updated networking system

--- src/network/GameClient.java
+++ src/network/GameClient.java
@@ -1,26 +1,53 @@
 package network;
 
-import java.net.*;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import network.message.*;
+import networkutils.*;
 import util.Exitable;
 
 public class GameClient implements Runnable, Exitable {
 	
 	protected DatagramSocket socket;
+	protected SocketAddress serverAddress;
 	
 	protected volatile boolean exit;
 	protected volatile boolean finished;
 	
-	public GameClient() {
-		
+	public GameClient(SocketAddress serverAddress) {
+		this.serverAddress = serverAddress;
 	}
 	
 	@Override
 	public void run() {
 		try {
 			socket = new DatagramSocket(new InetSocketAddress(InetAddress.getLocalHost(), 0));
+			DatagramInputStream input = new DatagramInputStream(socket);
+			DatagramOutputStream output = new DatagramOutputStream(socket, serverAddress);
+			output.write(new ServerInfoRequest().getBytes());
+			Message message = MessageParser.getMessage(input.readPacket());
+			if(message instanceof ServerInfoMessage) {
+				System.out.println("Server Info: " + ((ServerInfoMessage)message).getPlayerCount() + "/" + ((ServerInfoMessage)message).getPlayerCapacity() + " players");
+			}
+			
+			input.close();
+			output.close();
+			
 		} catch (SocketException e) {
 			e.printStackTrace();
 		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (UnknownMessageException e) {
+			e.printStackTrace();
+		} catch (InvalidMessageException e) {
 			e.printStackTrace();
 		}
 		
---
+++ src/network/message/ServerInfoRequest.java
@@ -0,0 +1,15 @@
+package network.message;
+
+import networkutils.ByteUtil;
+import networkutils.Message;
+
+public class ServerInfoRequest extends Message {
+
+	@Override
+	public byte[] getBytes() {
+		byte[] message = new byte[2];
+		ByteUtil.putShort(message, 0, (short)5);
+		return message;
+	}
+
+}
