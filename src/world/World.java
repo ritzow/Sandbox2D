7d0d9dfe67ecc63d537a284defd7dc1982758b2e
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-11-07 23:08:38 -0800

    Slightly optimized world rendering

--- src/world/World.java
+++ src/world/World.java
@@ -215,19 +215,12 @@ public class World implements Renderable, Serializable {
 		renderer.loadViewMatrix(true);
 		
 		int leftBound = 	Math.max(0, (int)Math.floor(renderer.getWorldViewportLeftBound()));
+		int bottomBound = 	Math.max(0, (int)Math.floor(renderer.getWorldViewportBottomBound()));
 		int rightBound = 	Math.min(foreground.getWidth(), (int)Math.ceil(renderer.getWorldViewportRightBound()) + 1);
 		int topBound = 		Math.min(foreground.getHeight(), (int)Math.ceil(renderer.getWorldViewportTopBound()) + 1);
-		int bottomBound = 	Math.max(0, (int)Math.floor(renderer.getWorldViewportBottomBound()));
 		
 		for(int row = bottomBound; row < topBound; row++) {
-			for(int column = leftBound; column < Math.min(foreground.getWidth(), rightBound); column++) {
-				
-				if(!foreground.isBlock(column, row) && background.isBlock(column, row)) {
-					Model blockModel = background.get(column, row).getModel();
-					renderer.loadOpacity(0.5f);
-					renderer.loadTransformationMatrix(column, row, 1, 1, 0);
-					blockModel.render();
-				}
+			for(int column = leftBound; column < rightBound; column++) {
 				
 				if(foreground.isBlock(column, row)) {
 					Model blockModel = foreground.get(column, row).getModel();
@@ -235,22 +228,26 @@ public class World implements Renderable, Serializable {
 					renderer.loadTransformationMatrix(column, row, 1, 1, 0);
 					blockModel.render();
 				}
+				
+				else if(background.isBlock(column, row)) {
+					Model blockModel = background.get(column, row).getModel();
+					renderer.loadOpacity(0.5f);
+					renderer.loadTransformationMatrix(column, row, 1, 1, 0);
+					blockModel.render();
+				}
 			}
 		}
 		
 		for(int i = 0; i < entities.size(); i++) {
-			Entity e = null;
-			try {
-				e = entities.get(i);
-			} catch(IndexOutOfBoundsException exception ) {
-				System.err.println("Attempted to access invalid index while rendering entities");
-			}
+			Entity e = entities.get(i);
 			
-			if(e != null && e.getPositionX() < renderer.getWorldViewportRightBound() + e.getWidth()/2 
-					&& e.getPositionX() > renderer.getWorldViewportLeftBound() - e.getWidth()/2 
-					&& e.getPositionY() < renderer.getWorldViewportTopBound() + e.getHeight()/2
-					&& e.getPositionY() > renderer.getWorldViewportBottomBound() - e.getHeight()/2)
-				e.render(renderer);
+			synchronized(e) { //TODO change so entity rendering doesn't rely on physical width and height?
+				if(e.getPositionX() < renderer.getWorldViewportRightBound() + e.getWidth()/2 
+				&& e.getPositionX() > renderer.getWorldViewportLeftBound() - e.getWidth()/2 
+				&& e.getPositionY() < renderer.getWorldViewportTopBound() + e.getHeight()/2 
+				&& e.getPositionY() > renderer.getWorldViewportBottomBound() - e.getHeight()/2)
+					e.render(renderer);
+			}
 		}
 	}
 }
