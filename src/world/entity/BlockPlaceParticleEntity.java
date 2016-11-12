31c6d7301108ee7203b63a72617c21ed66e630e8
Solomon Ritzow <solduck@gmail.com>
2016-11-12 00:07:17 -0800

    Created BlockPlaceParticleEntity class

---
+++ src/world/entity/BlockPlaceParticleEntity.java
@@ -0,0 +1,15 @@
+package world.entity;
+
+import world.block.Block;
+import world.entity.component.Graphics;
+
+import static util.Utility.MathUtility.randomFloat;
+
+public class BlockPlaceParticleEntity extends ParticleEntity {
+
+	private static final long serialVersionUID = -7531448862656734229L;
+
+	public BlockPlaceParticleEntity(Block block, float posX, float posY) {
+		super(new Graphics(block.getModel(), 1.0f, 0.3f, 0.3f, 0), posX, posY, randomFloat(-0.2f, 0.2f), randomFloat(0, 0.35f), randomFloat(-0.2f, 0.2f), 500, true);
+	}
+}
