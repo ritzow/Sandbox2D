54de9c449e0998880d5a5d9885e6f6bd20bd9488
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-11-21 16:37:28 -0800

    Renamed ElementManager to InterfaceElementManager and added WorldElementManager

---
+++ src/ui/WorldElementManager.java
@@ -0,0 +1,18 @@
+package ui;
+
+import graphics.ModelRenderer;
+
+public class WorldElementManager extends ElementManager {
+
+	@Override
+	public void render(ModelRenderer renderer) {
+		//TODO implement method 'render' in type 'Renderable'
+		
+	}
+
+	@Override
+	public void update() {
+		//TODO implement method 'update'
+	}
+
+}
