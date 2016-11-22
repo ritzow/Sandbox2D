11523fa803ed983717d7c9016b6c387030f12496
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-11-21 16:36:14 -0800

    Refactored/improved Server
    Packet processing
    client addition/removal
    Data sending

--- src/network/server/Server.java
+++ src/network/server/Server.java
@@ -1,26 +1,27 @@
 package network.server;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.net.*;
-import network.message.ServerConnectAcknowledgement;
-import network.message.ServerConnectRequest;
+import network.MessageHandler;
+import network.message.*;
 import networkutils.DatagramInputStream;
-import networkutils.DatagramOutputStream;
-import networkutils.InvalidMessageException;
+import util.ByteUtil;
 import util.Exitable;
 
-public class Server implements Runnable, Exitable {
+public class Server implements Runnable, Exitable, Closeable {
 
 	protected volatile boolean exit, finished;
 	protected DatagramSocket socket;
-	protected DatagramOutputStream[] clients;
+	protected SocketAddress[] clients;
+	protected MessageHandler messageHandler;
 
 	public Server() {
-		clients = new DatagramOutputStream[20];
+		clients = new SocketAddress[10];
 	}
 	
 	public Server(short capacity) {
-		clients = new DatagramOutputStream[capacity];
+		clients = new SocketAddress[capacity];
 	}
 
 	@Override
@@ -29,19 +30,40 @@ public class Server implements Runnable, Exitable {
 			socket = new DatagramSocket(new InetSocketAddress(InetAddress.getLocalHost(), 50000));
 			DatagramInputStream input = new DatagramInputStream(socket);
 			
-			while(!exit) {
-				DatagramPacket packet = new DatagramPacket(new byte[100], 100);
-				socket.receive(packet);
-				System.out.println("Server received message");
-				try {
-					new ServerConnectRequest(packet.getData());
-				} catch(InvalidMessageException e) {
-					System.out.println("Invalid message");
-					continue;
+			messageHandler = new MessageHandler() {
+
+				@Override
+				public void handle(ServerConnectRequest messsage, SocketAddress sender) {
+					try {
+						sendData(new ServerConnectAcknowledgment(clientsConnected() < clients.length).getBytes(), sender);
+						addClient(sender);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+				@Override
+				public void handle(ServerInfoRequest message, SocketAddress sender) {
+					
 				}
 				
-				byte[] response = new ServerConnectAcknowledgement(true).getBytes();
-				socket.send(new DatagramPacket(response, response.length, packet.getSocketAddress()));
+				@Override
+				public void handle(ClientInfoMessage message, SocketAddress sender) {
+					
+				}
+			};
+			
+			while(!exit) {
+				DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+				socket.receive(packet);
+				
+				try {
+					processPacket(packet);
+				} catch (InvalidMessageException e) {
+					continue;
+				} catch (UnknownMessageException e) {
+					continue;
+				}
 			}
 			
 			input.close();
@@ -58,24 +80,82 @@ public class Server implements Runnable, Exitable {
 			this.notifyAll();
 		}
 	}
+	
+	public synchronized void sendData(byte[] data, SocketAddress address) throws IOException {
+		socket.send(new DatagramPacket(data, data.length, address));
+	}
+	
+	@Override
+	public synchronized void close() throws IOException {
+		socket.close();
+		exit();
+	}
+	
+	protected boolean clientPresent(SocketAddress address) {
+		for(int i = 0; i < clients.length; i++) {
+			if(clients[i].equals(address)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
 
-	public int addClient(DatagramOutputStream output) {
+	protected boolean addClient(SocketAddress output) {
 		for(int i = 0; i < clients.length; i++) {
 			if(clients[i] == null) {
 				clients[i] = output;
-				return i;
+				return true;
 			}
 		}
 
-		return -1;
+		return false;
 	}
 
-	public void removeClient(DatagramOutputStream output) {
+	protected void removeClient(SocketAddress output) {
 		for(int i = 0; i < clients.length; i++) {
-			if(clients[i] == output) {
+			if(clients[i].equals(output)) {
 				clients[i] = null;
 			}
 		}
+	}
+	
+	protected int clientsConnected() {
+		int connected = 0;
+		for(int i = 0; i < clients.length; i++) {
+			if(clients[i] != null) {
+				connected++;
+			}
+		}
+		return connected;
+	}
+	
+	public void processPacket(DatagramPacket packet) throws UnknownMessageException, InvalidMessageException {
+		if(packet.getData().length < 2)
+			throw new InvalidMessageException();
+		try {
+			switch(ByteUtil.getShort(packet.getData(), 0)) {
+			case Protocol.SERVER_CONNECT_REQUEST:
+				messageHandler.handle(new ServerConnectRequest(), packet.getSocketAddress());
+				return;
+			case Protocol.CLIENT_INFO:
+				messageHandler.handle(new ClientInfoMessage(packet), packet.getSocketAddress());
+				return;
+			case Protocol.SERVER_INFO:
+				messageHandler.handle(new ServerInfoMessage(packet), packet.getSocketAddress());
+				return;
+			case Protocol.SERVER_INFO_REQUEST:
+				messageHandler.handle(new ServerInfoRequest(), packet.getSocketAddress());
+				return;
+			case Protocol.SERVER_CONNECT_ACKNOWLEDGMENT:
+				messageHandler.handle(new ServerConnectAcknowledgment(packet), packet.getSocketAddress());
+			default:
+				throw new UnknownMessageException();
+			}
+		} catch(InvalidMessageException e) {
+			System.err.println("Caught invalid message");
+		}
+
 	}
 
 	@Override
