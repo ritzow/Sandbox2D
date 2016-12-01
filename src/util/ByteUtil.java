5a6c38dacabf2b04a986a468ee48046e4c6f4489
Solomon Ritzow <solduck@gmail.com>
2016-11-30 19:19:53 -0800

    Added integer and object serialization/deserialization methods

--- src/util/ByteUtil.java
+++ src/util/ByteUtil.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.io.*;
+
 public final class ByteUtil {
 	
 	//Data composition from bytes
@@ -12,6 +14,10 @@ public final class ByteUtil {
 		return (short)((array[index] << 8) | (array[index + 1] & 0xff));
 	}
 	
+	public static int getInteger(byte[] array, int index) {
+		return ((array[index] & 255) << 24) | ((array[index + 1] & 255) << 16) | ((array[index + 2] & 255) << 8) | ((array[index + 3] & 255) << 0);
+	}
+	
 	public static float getFloat(byte a, byte b, byte c, byte d) {
 		return Float.intBitsToFloat(((a & 255) << 24) | ((b & 255) << 16) | ((c & 255) << 8) | ((d & 255) << 0));
 	}
@@ -22,6 +28,10 @@ public final class ByteUtil {
 	
 	public static boolean getBoolean(byte[] array, int index) {
 		return array[index] == 1 ? true : false;
+	}
+	
+	public static synchronized Object deserialize(byte[] data, int offset, int length) throws ClassNotFoundException, IOException {
+		return new ObjectInputStream(new ByteArrayInputStream(data, offset, length)).readObject();
 	}
 	
 	//Data decomposition to bytes
@@ -39,7 +49,21 @@ public final class ByteUtil {
 		array[index + 1] = (byte)(((int)value >>> 0) & 0xFF);
 	}
 	
+	public static void putInteger(byte[] array, int index, int value) {
+		array[index + 0] = (byte)(value >>> 24);
+		array[index + 1] = (byte)(value >>> 16);
+		array[index + 2] = (byte)(value >>> 8);
+		array[index + 3] = (byte)(value >>> 0);
+	}
+	
 	public static void putBoolean(byte[] array, int index, boolean b) {
 		array[index] = (byte) (b ? 1 : 0);
+	}
+	
+	public static synchronized byte[] serialize(Serializable object) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ObjectOutputStream ser = new ObjectOutputStream(out);
+		ser.writeObject(object);
+		return out.toByteArray();
 	}
 }
