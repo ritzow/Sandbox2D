e6d39a641dd55f2651927e9b14b02fcc0f8ee3ea
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2016-11-30 19:21:17 -0800

    Added generic Exception constructors to InvalidMessageException

--- src/network/message/InvalidMessageException.java
+++ src/network/message/InvalidMessageException.java
@@ -3,4 +3,26 @@ package network.message;
 public class InvalidMessageException extends Exception {
 	private static final long serialVersionUID = 1693805154920151682L;
 
+	public InvalidMessageException() {
+		super();
+	}
+
+	public InvalidMessageException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public InvalidMessageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public InvalidMessageException(String message) {
+		super(message);
+	}
+
+	public InvalidMessageException(Throwable cause) {
+		super(cause);
+	}
+	
+	
+
 }
