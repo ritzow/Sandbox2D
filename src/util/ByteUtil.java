9f4360325ba270dd04c72bf6b7a4dd9945f57961
Solomon Ritzow <solduck@gmail.com>
2016-12-14 17:30:47 -0800

    Updated serialization mechanisms
    Added custom object serialization system
    Replaced default object serializer with FST serialization (this may be
    removed in a future update)

--- src/util/ByteUtil.java
+++ src/util/ByteUtil.java
@@ -1,28 +1,55 @@
 package util;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 import org.nustaq.serialization.FSTConfiguration;
+import org.nustaq.serialization.FSTObjectInput;
+import org.nustaq.serialization.FSTObjectOutput;
 
 public final class ByteUtil {
 	
-	private static final FSTConfiguration serializer;
+	private static final FSTConfiguration fstConfig;
 	
 	static {
-		serializer = FSTConfiguration.createDefaultConfiguration();
-		serializer.registerClass(
-				world.block.Block.class, 
-				world.block.DirtBlock.class, 
-				world.block.GrassBlock.class, 
-				world.block.RedBlock.class, 
-				world.entity.Entity.class,
-				world.entity.ItemEntity.class,
-				world.entity.BlockPlaceParticleEntity.class,
-				world.entity.LivingEntity.class,
-				world.entity.ParticleEntity.class,
-				world.entity.Player.class
-		);
+		fstConfig = FSTConfiguration.createDefaultConfiguration();
+//		fstConfig.registerClass(
+//				world.block.Block.class, 
+//				world.block.DirtBlock.class, 
+//				world.block.GrassBlock.class, 
+//				world.block.RedBlock.class, 
+//				world.entity.Entity.class,
+//				world.entity.ItemEntity.class,
+//				world.entity.BlockPlaceParticleEntity.class,
+//				world.entity.LivingEntity.class,
+//				world.entity.ParticleEntity.class,
+//				world.entity.Player.class,
+//				world.entity.component.Inventory.class,
+//				world.BlockGrid.class,
+//				world.World.class,
+//				world.item.BlockItem.class
+//		);
+	}
+	
+	public static byte[] concatenate(byte[]... arrays) {
+		int length = 0;
+		for(int i = 0; i < arrays.length; i++) {
+			length += arrays[i].length;
+		}
+		
+		byte[] concatenated = new byte[length];
+		int index = 0;
+		for(int i = 0; i < arrays.length; i++) {
+			System.arraycopy(arrays[i], 0, concatenated, index, arrays[i].length);
+			index += arrays[i].length;
+		}
+		
+		return concatenated;
 	}
 	
 	//Data composition from bytes
@@ -62,10 +89,29 @@ public final class ByteUtil {
 	public static Object deserializeCompressed(byte[] data, int offset, int length) throws ClassNotFoundException, IOException {
 		ByteArrayInputStream in = new ByteArrayInputStream(data, offset, length);
 		InflaterInputStream inflater = new InflaterInputStream(in);
-		ObjectInputStream deserializer = new ObjectInputStream(inflater);
+		FSTObjectInput deserializer = fstConfig.getObjectInput(inflater);
 		Object object = deserializer.readObject();
-		deserializer.close();
+		inflater.close();
 		return object;
+	}
+	
+	/**
+	 * 
+	 * @param object the object packet, where the first four bytes are an integer representing the length of the class name String, followed by the class name
+	 * @param offset
+	 * @param length
+	 * @return
+	 * @throws ReflectiveOperationException
+	 */
+	public static <T extends Transportable> T deserializeTransportable(byte[] object) throws ReflectiveOperationException {
+		int classNameLength = ByteUtil.getInteger(object, 0);
+		@SuppressWarnings("unchecked")
+		Class<T> clazz = (Class<T>)(Class.forName(new String(object, 4, classNameLength)));
+		return deserializeTransportable(clazz, Arrays.copyOfRange(object, classNameLength + 4, object.length));
+	}
+	
+	public static <T extends Transportable> T deserializeTransportable(Class<T> type, byte[] data) throws ReflectiveOperationException {
+		return type.getConstructor(byte[].class).newInstance(data);
 	}
 	
 	//Data decomposition to bytes
@@ -99,42 +145,31 @@ public final class ByteUtil {
 		ObjectOutputStream serializer = new ObjectOutputStream(out);
 		serializer.writeObject(object);
 		serializer.close();
-		out.close();
 		return out.toByteArray();
 	}
 	
-	public static byte[] serializeCompressed(Object object) throws IOException { //TODO use FST Serialization?
+	public static byte[] serializeCompressed(Object object) throws IOException {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		DeflaterOutputStream compresser = new DeflaterOutputStream(out);
-		ObjectOutputStream serializer = new ObjectOutputStream(compresser);
+		FSTObjectOutput serializer = fstConfig.getObjectOutput(compresser);
 		serializer.writeObject(object);
-		serializer.close();
+		serializer.flush();
+		compresser.close();
 		return out.toByteArray();
 	}
 	
-//	private static final class ByteArrayFillOutputStream extends OutputStream {
-//		protected byte[] array;
-//		protected int index;
-//		
-//		public ByteArrayFillOutputStream(byte[] array, int offset) {
-//			this.array = array;
-//			this.index = offset;
-//		}
-//
-//		@Override
-//		public void write(int b) throws IOException {
-//			if(index >= array.length)
-//				throw new ArrayIndexOutOfBoundsException();
-//			array[index] = (byte)b;
-//			index++;
-//		}
-//
-//		@Override
-//		public void write(byte[] b, int off, int len) throws IOException {
-//			if(index >= array.length)
-//				throw new ArrayIndexOutOfBoundsException();
-//			System.arraycopy(b, off, array, index, len);
-//			index += len;
-//		}
-//	}
+	public static byte[] serializeTransportable(Transportable object) {
+		String name = object.getClass().getName();
+		byte[] nameBytes = name.getBytes();
+		byte[] objectBytes = object.toBytes();
+		byte[] packet = new byte[4 + nameBytes.length + objectBytes.length];
+		ByteUtil.putInteger(packet, 0, nameBytes.length);
+		System.arraycopy(nameBytes, 0, packet, 4, nameBytes.length);
+		System.arraycopy(objectBytes, 0, packet, nameBytes.length + 4, objectBytes.length);
+		return packet;
+	}
+	
+	public static byte[] serializeTransportableHeaderless(Transportable object) {
+		return object.toBytes();
+	}
 }
