97b40002e0e4096372e5f0fc58b45e9466590a96
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2017-01-18 17:44:50 -0800

    Updated netoworking classes
    Continued work on NetworkController, Client, and Server. Created
    MessageTimeoutException class.

--- src/ritzow/solomon/engine/network/Client.java
+++ src/ritzow/solomon/engine/network/Client.java
@@ -1,75 +1,127 @@
 package ritzow.solomon.engine.network;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.PortUnreachableException;
 import java.net.SocketAddress;
 import java.net.SocketException;
-import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
-import ritzow.solomon.engine.game.Lobby;
-import ritzow.solomon.engine.network.message.Protocol;
-import ritzow.solomon.engine.world.WorldManager;
+import ritzow.solomon.engine.util.ByteUtil;
+import ritzow.solomon.engine.world.World;
+import ritzow.solomon.engine.world.WorldUpdater;
 
 public final class Client extends NetworkController {
-	protected SocketAddress serverAddress;
-	protected WorldManager worldManager;
-	protected Lobby lobby;
-	
-	/** if the client sent a ServerInfoRequest to get server information **/
-	protected boolean requestedInfo;
+	protected SocketAddress server;
+	protected WorldUpdater worldManager;
+	protected boolean connected;
 	
 	public Client() throws SocketException, UnknownHostException {
 		super(new InetSocketAddress(InetAddress.getLocalHost(), 0));
 	}
 	
-	/**
-	 * Connect to a specified SocketAddress using a ServerConnectRequest
-	 * @param serverAddress the socket address of the server
-	 * @param attempts number of times to resend the connection request
-	 * @param timeout the total amount of time in milliseconds to wait for the server to respond
-	 * @return whether or not the server responded and accepted the client's connection
-	 * @throws IOException if the socket throws an IOException
-	 */
-	public synchronized boolean connectToServer(SocketAddress serverAddress, int attempts, int timeout) throws IOException {
-		if(attempts == 0)
-			throw new RuntimeException("Number of connection attempts cannot be zero");
-		else if(timeout != 0 && timeout < attempts)
-			throw new RuntimeException("Specified connection timeout is too small");
-		setTimeout(timeout/attempts);
-		byte[] packet = Protocol.constructServerConnectRequest();
-		send(packet, serverAddress);
-		DatagramPacket response = new DatagramPacket(new byte[7], 7);
-		long startTime = System.currentTimeMillis();
-		int attemptsRemaining = attempts;
-		while(attemptsRemaining >= 0 && (System.currentTimeMillis() - startTime < timeout || timeout == 0)) {
-			try {
-				socket.receive(response);
-				if(response.getSocketAddress().equals(serverAddress)) {
-					if(Protocol.deconstructServerConnectResponse(response.getData())) {
-						this.serverAddress = response.getSocketAddress();
-						return true;
-					} else {
-						return false;
-					}
-				} else {
-					continue;
-				}
-			} catch(PortUnreachableException e) {
-				return false;
-			} catch(SocketTimeoutException e) {
-				if(--attemptsRemaining > 0)
-					send(packet, serverAddress);
-				continue;
-			}
-		}
-		return false;
-	}
-
+	private byte[][] worldPackets;
+	
 	@Override
 	protected void processMessage(int messageID, short protocol, SocketAddress sender, byte[] data) {
+		System.out.println("Client received message of ID " + messageID + " and type " + protocol);
 		
+		if(sender.equals(server)) {
+			if(protocol == Protocol.SERVER_CONNECT_ACKNOWLEDGMENT) {
+				connected = ByteUtil.getBoolean(data, 0);
+				synchronized(server) {
+					server.notifyAll(); //notify the connectTo method that the client connected to the server successfully.
+				}
+			}
+			
+			else if(protocol == Protocol.WORLD_HEAD) {
+				worldPackets = new byte[ByteUtil.getInteger(data, 0)][];
+			}
+			
+			else if(protocol == Protocol.WORLD) {
+				for(int i = 0; i < worldPackets.length; i++) {
+					if(worldPackets[i] == null) {
+						worldPackets[i] = data;
+						
+						if(i == worldPackets.length - 1) { //received final packet, create the world
+							World world = Protocol.deconstructWorldPackets(worldPackets);
+							new Thread(worldManager = new WorldUpdater(world), "Client World Manager").start();
+						}
+						
+						break;
+					}
+				}
+			}
+		}
 	}
+	
+	public World getWorld() {
+		return worldManager == null ? null : worldManager.getWorld();
+	}
+	
+	public boolean connectTo(SocketAddress server, int timeout) throws IOException {
+		if(this.server == null) {
+			if(this.isSetupComplete() && !this.isFinished()) {
+				this.server = server;
+				send(Protocol.constructServerConnectRequest(), server);
+				synchronized(server) {
+					try {
+						this.wait(timeout);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				return connected;
+			} else {
+				throw new RuntimeException("the client must be running to connect to a server");
+			}
+			
+		} else {
+			send(Protocol.constructClientDisconnect(0), server); //TODO what about handling timeouts, etc.?
+			return connectTo(server, timeout);
+		}
+	}
+	
+//	/**
+//	 * Connect to a specified SocketAddress using a ServerConnectRequest
+//	 * @param serverAddress the socket address of the server
+//	 * @param attempts number of times to resend the connection request
+//	 * @param timeout the total amount of time in milliseconds to wait for the server to respond
+//	 * @return whether or not the server responded and accepted the client's connection
+//	 * @throws IOException if the socket throws an IOException
+//	 */
+//	public synchronized boolean connectToServer(SocketAddress serverAddress, int attempts, int timeout) throws IOException {
+//		if(attempts == 0)
+//			throw new RuntimeException("Number of connection attempts cannot be zero");
+//		else if(timeout != 0 && timeout < attempts)
+//			throw new RuntimeException("Specified connection timeout is too small");
+//		setTimeout(timeout/attempts);
+//		byte[] packet = Protocol.constructServerConnectRequest();
+//		DatagramPacket datagram = new DatagramPacket(packet, packet.length, serverAddress);
+//		socket.send(datagram);
+//		DatagramPacket response = new DatagramPacket(new byte[7], 7);
+//		long startTime = System.currentTimeMillis();
+//		int attemptsRemaining = attempts;
+//		while(attemptsRemaining >= 0 && (System.currentTimeMillis() - startTime < timeout || timeout == 0)) {
+//			try {
+//				socket.receive(response);
+//				if(response.getSocketAddress().equals(serverAddress)) {
+//					if(Protocol.deconstructServerConnectResponse(response.getData())) {
+//						this.server = response.getSocketAddress();
+//						return true;
+//					} else {
+//						return false;
+//					}
+//				} else {
+//					continue;
+//				}
+//			} catch(PortUnreachableException e) {
+//				return false;
+//			} catch(SocketTimeoutException e) {
+//				if(--attemptsRemaining > 0)
+//					socket.send(datagram);
+//				continue;
+//			}
+//		}
+//		return false;
+//	}
 }
--- src/ritzow/solomon/engine/network/NetworkController.java
+++ src/ritzow/solomon/engine/network/NetworkController.java
@@ -1,20 +1,11 @@
 package ritzow.solomon.engine.network;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketAddress;
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
+import java.net.*;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import ritzow.solomon.engine.network.message.MessageHandle;
-import ritzow.solomon.engine.network.message.Protocol;
 import ritzow.solomon.engine.util.ByteUtil;
 import ritzow.solomon.engine.util.Exitable;
 import ritzow.solomon.engine.util.Installable;
@@ -36,30 +27,33 @@ public abstract class NetworkController implements Installable, Runnable, Exitab
 	
 	protected abstract void processMessage(int messageID, short protocol, SocketAddress sender, byte[] data);
 	
-	public void setTimeout(int milliseconds) throws SocketException { //TODO find different solution in client connect method
-		socket.setSoTimeout(milliseconds);
-	}
-	
 	public void send(byte[] packet, SocketAddress address) throws IOException {
-		socket.send(new DatagramPacket(packet, packet.length, address));
+		if(packet.length < 6)
+			throw new RuntimeException("invalid packet");
+		DatagramPacket datagram = new DatagramPacket(packet, packet.length, address);
+		if(Protocol.isReliable(ByteUtil.getShort(packet, 4))) {
+			if(!this.isSetupComplete() || this.isFinished()) {
+				throw new RuntimeException("the network controller must be running to send packets reliably");	
+			} else {
+				try {
+					MessageHandle handle = new MessageHandle(ByteUtil.getInteger(packet, 0));
+					handles.add(handle);
+					
+					do {
+						socket.send(datagram);
+					} while(!handle.waitForResponse());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		} else {
+			socket.send(datagram);
+		}
 	}
 	
-	public void sendReliable(byte[] packet, SocketAddress address) throws IOException {
-		if(packet.length < 4)
-			throw new RuntimeException("invalid packet");
-		else if(!this.isSetupComplete() || this.isFinished())
-			throw new RuntimeException("the network controller must be running to send packets reliably");
-		try {
-			
-			MessageHandle handle = new MessageHandle(ByteUtil.getInteger(packet, 0));
-			handles.add(handle);
-			send(packet, address);
-			
-			do {
-				send(packet, address);
-			} while(!handle.waitForResponse(500));
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+	public void send(byte[][] packets, SocketAddress address) throws IOException {
+		for(byte[] a : packets) {
+			send(a, address);
 		}
 	}
 	
@@ -85,8 +79,9 @@ public abstract class NetworkController implements Installable, Runnable, Exitab
 	
 	@Override
 	public void run() {
-		ExecutorService dispatcher = Executors.newCachedThreadPool();
+		ExecutorService dispatcher = Executors.newFixedThreadPool(10); //TODO experiment with thread pool types
 		DatagramPacket buffer = new DatagramPacket(new byte[Protocol.MAX_MESSAGE_LENGTH], Protocol.MAX_MESSAGE_LENGTH);
+		List<Integer> received = new ArrayList<Integer>();
 		
 		synchronized(this) {
 			setupComplete = true;
@@ -98,33 +93,43 @@ public abstract class NetworkController implements Installable, Runnable, Exitab
 				socket.setSoTimeout(0);
 				socket.receive(buffer);
 				if(buffer.getLength() < 6)
-					continue; //it's a troll, just ignore it.
+					continue; //ignore received messages that are too short
 				
-				int protocol = ByteUtil.getShort(buffer.getData(), buffer.getOffset() + 4);
+//				if(Math.random() < 0.5) //currently set at 50% chance that message is not received
+//					continue;
+
+				final int messageID = ByteUtil.getInteger(buffer.getData(), buffer.getOffset());
+				final short protocol = ByteUtil.getShort(buffer.getData(), buffer.getOffset() + 4);
 				
-				if(protocol == Protocol.RESPONSE_MESSAGE) {
-					int messageID = ByteUtil.getInteger(buffer.getData(), buffer.getOffset() + 6); //message id that the received message is a response to
+				if(protocol == Protocol.RESPONSE_MESSAGE) { //notify any send call waiting for this response
+					int receivedID = ByteUtil.getInteger(buffer.getData(), buffer.getOffset() + 6); //message id that the received message is a response to
 					
-					ListIterator<MessageHandle> iterator = handles.listIterator();
-					while(iterator.hasNext()) {
-						MessageHandle h = iterator.next();
-						if(h.getMessageID() == messageID) {
+					for(MessageHandle h : handles) {
+						if(receivedID == h.getMessageID()) {
 							h.notifyReceived();
-							iterator.remove(); //TODO this doesn't account for a timeout, message handles will stay in the list
 							break;
 						}
 					}
-				}
-				
-				dispatcher.execute(new Runnable() {
-					//store the packet data because it wont be available once the socket calls receive again (which happens immediately)
-					byte[] packet = Arrays.copyOfRange(buffer.getData(), buffer.getOffset(), buffer.getOffset() + buffer.getLength());
-					SocketAddress address = buffer.getSocketAddress();
 					
-					public void run() {
-						processMessage(ByteUtil.getInteger(packet, 0), ByteUtil.getShort(packet, 4), address, Arrays.copyOfRange(packet, 6, packet.length));
+					//TODO remove old message handles from list
+					
+				} else {
+					if(Protocol.isReliable(protocol)) { //responsd to reliable messages
+						if(received.contains(messageID)) {
+							send(Protocol.constructMessageResponse(0, messageID), buffer.getSocketAddress()); continue; //don't reprocess packet
+						} else {
+							received.add(messageID);
+						}
+						//TODO remove old received message IDs from list
 					}
-				});
+					
+					/* Creates a copy of the SocketAddress and data so they can be used on the processor's thread,
+					 * the messageID and protocol are local variables only so they don't need to be copied.
+					 * Executes the processMessage method on another thread. */
+					SocketAddress sender = buffer.getSocketAddress();
+					byte[] data = Arrays.copyOfRange(buffer.getData(), 6, buffer.getLength());
+					dispatcher.execute(() -> processMessage(messageID, protocol, sender, data));
+				}
 			} catch(SocketTimeoutException e) {
 				continue;
 			} catch(SocketException e) {
