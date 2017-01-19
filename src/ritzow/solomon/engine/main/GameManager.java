7881ef9e8ae6972c38e26e9fc14cad9d0b3281b2
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2017-01-18 17:43:23 -0800

    Cleaned and reordered GameManager to prepare for network world transfer

--- src/ritzow/solomon/engine/main/GameManager.java
+++ src/ritzow/solomon/engine/main/GameManager.java
@@ -19,7 +19,6 @@ import ritzow.solomon.engine.input.handler.KeyHandler;
 import ritzow.solomon.engine.input.handler.WindowCloseHandler;
 import ritzow.solomon.engine.network.Client;
 import ritzow.solomon.engine.network.Server;
-import ritzow.solomon.engine.network.message.Protocol;
 import ritzow.solomon.engine.resource.Models;
 import ritzow.solomon.engine.util.ByteUtil;
 import ritzow.solomon.engine.util.Utility.Synchronizer;
@@ -48,20 +47,8 @@ public final class GameManager implements Runnable, WindowCloseHandler, KeyHandl
 	public void run() {
 		EventManager eventManager = this.eventManager;
 		
-		//wait for the Display, and thus InputManager, to be created, then link the game manager to the display so that the escape button and x button exit the game
-		Synchronizer.waitForSetup(eventManager);
-		eventManager.getDisplay().getInputManager().getWindowCloseHandlers().add(this);
-		eventManager.getDisplay().getInputManager().getKeyHandlers().add(this);
-		
-		//start the graphics manager, which will load all models into OpenGL and setup the OpenGL context.
-		GraphicsManager graphicsManager = new GraphicsManager(eventManager.getDisplay());
-		new Thread(graphicsManager, "Graphics Manager").start();
-		
 		//start OpenAL and load audio files.
 		AudioSystem.start();
-		
-		//wait for the graphics manager to finish setup.
-		Synchronizer.waitForSetup(graphicsManager);
 		
 		/* TODO figure out how to implement "air"/refactor block system so that blocks like air (non-physical blocks)...
 		 * won't cause weird bugs by refactoring based on the assumption that any given block wont be physical/have a model/collide/etc.
@@ -73,13 +60,23 @@ public final class GameManager implements Runnable, WindowCloseHandler, KeyHandl
 		File saveFile = new File("data/worlds/testWorld.dat");
 		World world;
 		
-		if(!saveFile.exists() || saveFile.length() == 0) {
+		if(saveFile.exists() && saveFile.length() > 0) {
+			try(FileInputStream in = new FileInputStream(saveFile)) {
+				byte[] data = new byte[(int)saveFile.length()];
+				in.read(data);
+				System.out.print("Loading world... ");
+				world = (World)ByteUtil.deserialize(ByteUtil.decompress(data));
+				System.out.println("done!");
+			} catch(IOException | ReflectiveOperationException e) {
+				e.printStackTrace();
+				world = null;
+			}
+		} else {
 			System.out.print("Creating world... ");
-			world = new World(750, 500);
+			world = new World(500, 200);
 			for(int column = 0; column < world.getForeground().getWidth(); column++) {
 				double height = world.getForeground().getHeight()/2;
 				height += (Math.sin(column * 0.1f) + 1) * (world.getForeground().getHeight() - height) * 0.05f;
-				
 				for(int row = 0; row < height; row++) {
 					if(Math.random() < 0.007) {
 						world.getForeground().set(column, row, new RedBlock());
@@ -92,80 +89,74 @@ public final class GameManager implements Runnable, WindowCloseHandler, KeyHandl
 				world.getBackground().set(column, (int)height, new DirtBlock());
 			}
 			System.out.println("world created.");
-		} else {
-			try(FileInputStream in = new FileInputStream(saveFile)) {
-				byte[] data = new byte[(int)saveFile.length()];
-				in.read(data);
-				System.out.print("Loading world... ");
-				world = (World)ByteUtil.deserialize(ByteUtil.decompress(data));
-				System.out.println("done!");
-			} catch(IOException | ReflectiveOperationException e) {
-				e.printStackTrace();
-				world = null;
-			}
 		}
 		
-		//create the player's character at the middle top of the world
-		Player player = new Player();
-		player.setPositionX(world.getForeground().getWidth()/2);
-		for(int i = world.getForeground().getHeight() - 2; i > 1; i--) {
-			if(world.getForeground().get(player.getPositionX(), i) == null && world.getForeground().get(player.getPositionX(), i + 1) == null &&
-				world.getForeground().get(player.getPositionX(), i - 1) != null) {
-				player.setPositionY(i);
-				break;
-			}
-		} world.add(player);
+		//wait for the Display, and thus InputManager, to be created, then link the game manager to the display so that the escape button and x button exit the game
+		Synchronizer.waitForSetup(eventManager);
+		eventManager.getDisplay().getInputManager().getWindowCloseHandlers().add(this);
+		eventManager.getDisplay().getInputManager().getKeyHandlers().add(this);
+		
+		//start the graphics manager, which will load all models into OpenGL and setup the OpenGL context.
+		GraphicsManager graphicsManager = new GraphicsManager(eventManager.getDisplay());
+		new Thread(graphicsManager, "Graphics Manager").start();
+		
+		//wait for the graphics manager to finish setup.
+		Synchronizer.waitForSetup(graphicsManager);
 		
 		//create the client update manager and link it with the window events
 		ClientUpdateManager clientUpdateManager = new ClientUpdateManager();
 		clientUpdateManager.link(eventManager.getDisplay().getInputManager());
-		
-		//create player controllers so the user can "play the game", for testing purposes
-		Controller[] controllers = new Controller[] {
-				new EntityController(player, world, 0.2f, false),
-				new InteractionController(player, world, graphicsManager.getRenderer().getCamera(), 200, false),
-				new TrackingCameraController(graphicsManager.getRenderer().getCamera(), player, 0.005f, 0.05f, 0.6f)
-		};
-		
-		for(Controller c : controllers) {
-			c.link(eventManager.getDisplay().getInputManager());
-			clientUpdateManager.getUpdatables().add(c);
-		}
-		
-		//start the client update manager
 		new Thread(clientUpdateManager, "Client Updater").start();
 		
 		//Add the background and world to the renderer
 		graphicsManager.getRenderables().add(new Background(Models.CLOUDS));
-		graphicsManager.getRenderables().add(world);
 		
 		//perform a test of the client and server system (work in progress)
-		Client client;
-		Server server;
+		Client client = null;
+		Server server = null;
 		
 		try {
 			client = new Client();
-			server = new Server(20);
+			server = new Server();
+			new Thread(client, "Game Client").start();
 			new Thread(server, "Game Server").start();
+			server.startWorld(world);
 			Synchronizer.waitForSetup(server);
 			SocketAddress serverAddress = server.getSocketAddress();
-			if(client.connectToServer(serverAddress, 1, 1000)) {
-				new Thread(client, "Game Client").start();
+			if(client.connectTo(serverAddress, 1000)) {
 				System.out.println("Client connected to " + serverAddress);
-				server.startWorld(world); //temporary, so that I can play the game, should be handled in Server class automatically on startup
-				client.sendReliable(Protocol.constructDummy(23232433), serverAddress);
-				System.out.println("message received");
+				
+				//create the player's character at the middle top of the world
+				Player player = new Player();
+				player.setPositionX(world.getForeground().getWidth()/2);
+				for(int i = world.getForeground().getHeight() - 2; i > 1; i--) {
+					if(world.getForeground().get(player.getPositionX(), i) == null && world.getForeground().get(player.getPositionX(), i + 1) == null &&
+						world.getForeground().get(player.getPositionX(), i - 1) != null) {
+						player.setPositionY(i);
+						break;
+					}
+				}
+				
+				//create player controllers so the user can "play the game", for testing purposes
+				Controller[] controllers = new Controller[] {
+						new EntityController(player, world, 0.2f, false),
+						new InteractionController(player, world, graphicsManager.getRenderer().getCamera(), 200, false),
+						new TrackingCameraController(graphicsManager.getRenderer().getCamera(), player, 0.005f, 0.05f, 0.6f)
+				};
+				
+				for(Controller c : controllers) {
+					c.link(eventManager.getDisplay().getInputManager());
+					clientUpdateManager.getUpdatables().add(c);
+				}
+				
+				//TODO server needs to send world to client and client needs to set everything up.
+				
 			} else {
 				System.out.println("Client failed to connect to " + serverAddress);
-				exit();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-			client = null;
-			server = null;
 		}
-		
-		final World lastWorld = server.getWorld();
 		
 		//display the window!
 		eventManager.setReadyToDisplay();
@@ -189,21 +180,13 @@ public final class GameManager implements Runnable, WindowCloseHandler, KeyHandl
 			eventManager.exit();
 			
 			//make sure the client and server are closed before saving the world
+			Synchronizer.waitUntilFinished(client);
 			Synchronizer.waitUntilFinished(server);
 			
 			//save the world to the file "data/worlds/testWorld.dat"
-			
-			if(!saveFile.exists()) {
-				try {
-					saveFile.createNewFile();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			
 			try(FileOutputStream out = new FileOutputStream(saveFile)) {
 				System.out.print("Saving world... ");
-				byte[] serialized = ByteUtil.compress(ByteUtil.serialize(lastWorld));
+				byte[] serialized = ByteUtil.compress(ByteUtil.serialize(server.getWorld()));
 				out.write(serialized);
 				out.getChannel().truncate(serialized.length);
 				System.out.println("world saved to " + serialized.length + " bytes");
@@ -212,7 +195,6 @@ public final class GameManager implements Runnable, WindowCloseHandler, KeyHandl
 			}
 			
 			//make sure the client update manager and event manager are closed before exiting the "game" thread
-			Synchronizer.waitUntilFinished(client);
 	 		Synchronizer.waitUntilFinished(clientUpdateManager);
 			Synchronizer.waitUntilFinished(eventManager);
 		}
