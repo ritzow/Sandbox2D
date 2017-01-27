164f0de5d65ee3315fc7af9d3c102210f933f86b
Solomon Ritzow <solduck@gmail.com>
2017-01-26 22:17:14 -0800

    Created "reminder" class for abstracting user actions from keypresses

---
+++ src/ritzow/solomon/engine/input/UserActions.java
@@ -0,0 +1,16 @@
+package ritzow.solomon.engine.input;
+
+public final class UserActions {
+	/** Player world-based action **/
+	public static final short
+		BEGIN_MOVE_UP = 0,
+		END_MOVE_UP = 1,
+		BEGIN_MOVE_LEFT = 2,
+		END_MOVE_LEFT = 3,
+		BEGIN_MOVE_RIGHT = 4,
+		END_MOVE_RIGHT = 5,
+		BEGIN_MOVE_DOWN = 6,
+		END_MOVE_DOWN = 7,
+		BEGIN_PRIMARY_ACTION = 8,
+		END_PRIMARY_ACTION = 9;
+}
