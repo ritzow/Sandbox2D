f30a29aa9e8492e946e097f9283d85afb925ef17
Solomon Ritzow <solduck@gmail.com>
2017-01-26 22:08:45 -0800

    Renamed multiple classes

--- src/ritzow/solomon/engine/world/block/Block.java
+++ src/ritzow/solomon/engine/world/block/Block.java
@@ -1,8 +1,8 @@
 package ritzow.solomon.engine.world.block;
 
-import static ritzow.solomon.engine.audio.AudioSystem.playSound;
 import static ritzow.solomon.engine.util.Utility.MathUtility.randomFloat;
 
+import ritzow.solomon.engine.audio.Audio;
 import ritzow.solomon.engine.graphics.Model;
 import ritzow.solomon.engine.resource.Sounds;
 import ritzow.solomon.engine.util.Transportable;
@@ -38,11 +38,11 @@ public abstract class Block implements Transportable {
 		drop.setVelocityX(-0.2f + ((float)Math.random() * (0.4f)));
 		drop.setVelocityY((float)Math.random() * (0.35f));
 		world.add(drop);
-		playSound(Sounds.BLOCK_BREAK, x, y, drop.getVelocityX(), drop.getVelocityY(), randomFloat(0.75f, 1.5f), randomFloat(0.75f, 1.5f));
+		Audio.playSound(Sounds.BLOCK_BREAK, x, y, drop.getVelocityX(), drop.getVelocityY(), randomFloat(0.75f, 1.5f), randomFloat(0.75f, 1.5f));
 	}
 	
 	public void onPlace(World world, float x, float y) {
-		playSound(Sounds.BLOCK_PLACE, x, y, 0, 0, 1, randomFloat(0.9f, 1.1f));
+		Audio.playSound(Sounds.BLOCK_PLACE, x, y, 0, 0, 1, randomFloat(0.9f, 1.1f));
 		for(int i = 0; i < 3; i++) {
 			world.add(new BlockPlaceParticleEntity(this, x, y));
 		}
