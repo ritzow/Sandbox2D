72486aac1347db167611ab9fa2b3f2894bc4fb27
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2017-02-20 15:37:59 -0800

    Deleted unused classes

--- src/ritzow/solomon/engine/main/StartClientServer.java
+++
@@ -1,8 +0,0 @@
-package ritzow.solomon.engine.main;
-
-public class StartClientServer {
-	public static void main(String[] args) throws java.io.IOException {
-		StartServer.main(args);
-		StartClient.main(args);
-	}
-}
