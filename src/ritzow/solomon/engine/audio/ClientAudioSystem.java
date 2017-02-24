fab81f1ebcd0805d5e035d29d0d02ca51bde9d42
Solomon Ritzow <solduck@gmail.com>
2017-02-23 16:19:29 -0800

    Updated audio system

--- src/ritzow/solomon/engine/audio/ClientAudioSystem.java
+++ src/ritzow/solomon/engine/audio/ClientAudioSystem.java
@@ -23,42 +23,28 @@ import static org.lwjgl.openal.ALC10.alcDestroyContext;
 import static org.lwjgl.openal.ALC10.alcGetInteger;
 import static org.lwjgl.openal.ALC10.alcMakeContextCurrent;
 import static org.lwjgl.openal.ALC10.alcOpenDevice;
-import static org.lwjgl.openal.ALC11.ALC_ALL_DEVICES_SPECIFIER;
 import static org.lwjgl.openal.ALC11.ALC_MONO_SOURCES;
 
-import java.io.File;
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
-import java.util.List;
 import org.lwjgl.openal.AL;
 import org.lwjgl.openal.ALC;
 import org.lwjgl.openal.ALCCapabilities;
-import org.lwjgl.openal.ALCapabilities;
-import org.lwjgl.openal.ALUtil;
 
-public class ClientAudioSystem implements AudioSystem {
-	
-	private static int[] sources;
-	private static long context;
-	private static long device;
+public final class ClientAudioSystem implements AudioSystem {
+	private final int[] sources;
+	private final long context;
+	private final long device;
 	
 	public ClientAudioSystem() {
 		device = alcOpenDevice((ByteBuffer)null);
 		context = alcCreateContext(device, (IntBuffer)null);
 		ALCCapabilities alcCaps = ALC.createCapabilities(device);
 		alcMakeContextCurrent(context);
-		ALCapabilities alCaps = AL.createCapabilities(alcCaps);
 		
-		if(!alCaps.OpenAL11) {
+		if(!AL.createCapabilities(alcCaps).OpenAL11) {
 			System.err.println("OpenAL 1.1 not supported");
 			shutdown();
-		}
-		
-		try {
-			Sounds.loadAll(new File("resources/assets/audio"));
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 		
 		sources = new int[alcGetInteger(device, ALC_MONO_SOURCES)];
@@ -79,7 +65,7 @@ public class ClientAudioSystem implements AudioSystem {
 		for(int i = 0; i < sources.length; i++) {
 			int state = alGetSourcei(sources[i], AL_SOURCE_STATE);
 			if(state == AL_STOPPED || state == AL_INITIAL) {
-				alSourcei(sources[i], AL_BUFFER, sound); //TODO what is buffer id??
+				alSourcei(sources[i], AL_BUFFER, sound);
 				alSource3f(sources[i], AL_POSITION, x, y, 0);
 				alSource3f(sources[i], AL_VELOCITY, velocityX, velocityY, 0);
 				alSourcef(sources[i], AL_GAIN, gain);
@@ -94,20 +80,11 @@ public class ClientAudioSystem implements AudioSystem {
 		System.out.println("client global sound playing not implemented");
 	}
 
-	public void setVolume(float gain) {
+	public synchronized void setVolume(float gain) {
 		alListenerf(AL_GAIN, gain);
 	}
 
-	public void setRelativePosition(float x, float y) {
+	public synchronized void setRelativePosition(float x, float y) {
 		alListener3f(AL_POSITION, x, y, 0);
 	}
-	
-	public void listDevices() {
-		List<String> deviceList = ALUtil.getStringList(0, ALC_ALL_DEVICES_SPECIFIER);
-		System.out.println("OpenAL Device List:");
-		for(String device : deviceList) {
-			System.out.println("	" + device);
-		}
-	}
-	
 }
