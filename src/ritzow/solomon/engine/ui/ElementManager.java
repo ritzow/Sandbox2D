441cafd67ff4ff70a7247550eaf2017f8108374f
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2017-02-23 16:26:15 -0800

    Removed unused/badly implemented user interface classes

--- src/ritzow/solomon/engine/ui/ElementManager.java
+++
@@ -1,8 +0,0 @@
-package ritzow.solomon.engine.ui;
-
-import ritzow.solomon.engine.graphics.Renderable;
-import ritzow.solomon.engine.util.Updatable;
-
-public abstract class ElementManager implements Renderable, Updatable {
-	
-}
