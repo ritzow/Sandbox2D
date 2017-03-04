bf0078f1825410fad5f13dd1029ca45a4519b837
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2017-03-03 19:26:38 -0800

    Inlined import statements in Controls class

--- src/ritzow/solomon/engine/input/Controls.java
+++ src/ritzow/solomon/engine/input/Controls.java
@@ -1,24 +1,13 @@
 package ritzow.solomon.engine.input;
 
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_DOWN;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_E;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_F;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_KP_ADD;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_KP_SUBTRACT;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_LEFT;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_RIGHT;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_UP;
-
-/** Some constants for GLFW keycodes used in controllers **/
 public final class Controls {
-	public static final int KEYBIND_UP = 			GLFW_KEY_UP;
-	public static final int KEYBIND_DOWN = 			GLFW_KEY_DOWN;
-	public static final int KEYBIND_LEFT = 			GLFW_KEY_LEFT;
-	public static final int KEYBIND_RIGHT = 		GLFW_KEY_RIGHT;
-	public static final int KEYBIND_INCREASEZOOM = 	GLFW_KEY_KP_ADD;
-	public static final int KEYBIND_DECREASEZOOM = 	GLFW_KEY_KP_SUBTRACT;
-	public static final int KEYBIND_QUIT = 			GLFW_KEY_ESCAPE;
-	public static final int KEYBIND_FULLSCREEN =	GLFW_KEY_F;
-	public static final int KEYBIND_ACTIVATE =		GLFW_KEY_E;
+	public static final int KEYBIND_UP = 			org.lwjgl.glfw.GLFW.GLFW_KEY_UP;
+	public static final int KEYBIND_DOWN = 			org.lwjgl.glfw.GLFW.GLFW_KEY_DOWN;
+	public static final int KEYBIND_LEFT = 			org.lwjgl.glfw.GLFW.GLFW_KEY_LEFT;
+	public static final int KEYBIND_RIGHT = 		org.lwjgl.glfw.GLFW.GLFW_KEY_RIGHT;
+	public static final int KEYBIND_INCREASEZOOM = 	org.lwjgl.glfw.GLFW.GLFW_KEY_KP_ADD;
+	public static final int KEYBIND_DECREASEZOOM = 	org.lwjgl.glfw.GLFW.GLFW_KEY_KP_SUBTRACT;
+	public static final int KEYBIND_QUIT = 			org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+	public static final int KEYBIND_FULLSCREEN =	org.lwjgl.glfw.GLFW.GLFW_KEY_F11;
+	public static final int KEYBIND_ACTIVATE =		org.lwjgl.glfw.GLFW.GLFW_KEY_E;
 }
