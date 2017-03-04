fbeb75406c3411697c1fecd57761922d1f202e9d
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2017-03-03 19:26:13 -0800

    Added input UserControl class for future use

---
+++ src/ritzow/solomon/engine/input/UserControl.java
@@ -0,0 +1,13 @@
+package ritzow.solomon.engine.input;
+
+public class UserControl { //TODO should this be abstract instead? and use subclasses instead of type constant
+	protected final int type;
+	protected final int descriptor;
+	
+	public UserControl(int type, int descriptor) {
+		super();
+		this.type = type;
+		this.descriptor = descriptor;
+	}
+}
+
