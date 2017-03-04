d2d4c6a7c48775fb6f0f9def8b43113b22220eab
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2017-03-04 14:39:48 -0800

    Moved item dropping code and improved item slot selection
    Moved from PlayerEntity to InteractionController

--- src/ritzow/solomon/engine/input/controller/InteractionController.java
+++ src/ritzow/solomon/engine/input/controller/InteractionController.java
@@ -1,7 +1,9 @@
 package ritzow.solomon.engine.input.controller;
 
 import org.lwjgl.glfw.GLFW;
+import ritzow.solomon.engine.audio.Sounds;
 import ritzow.solomon.engine.graphics.Camera;
+import ritzow.solomon.engine.input.Controls;
 import ritzow.solomon.engine.input.InputManager;
 import ritzow.solomon.engine.input.handler.CursorPosHandler;
 import ritzow.solomon.engine.input.handler.FramebufferSizeHandler;
@@ -9,6 +11,7 @@ import ritzow.solomon.engine.input.handler.KeyHandler;
 import ritzow.solomon.engine.input.handler.MouseButtonHandler;
 import ritzow.solomon.engine.util.Updatable;
 import ritzow.solomon.engine.world.base.World;
+import ritzow.solomon.engine.world.entity.ItemEntity;
 import ritzow.solomon.engine.world.entity.PlayerEntity;
 import ritzow.solomon.engine.world.item.BlockItem;
 import ritzow.solomon.engine.world.item.Item;
@@ -123,50 +126,56 @@ public final class InteractionController implements Controller, MouseButtonHandl
 
 	@Override
 	public void keyboardButton(int key, int scancode, int action, int mods) {
-		if(key == GLFW.GLFW_KEY_L && action == GLFW.GLFW_PRESS) {
-			player.dropSelectedItem(world);
-		}
-		
-		else if(key == GLFW.GLFW_KEY_C && action == GLFW.GLFW_PRESS) {
-			
-		}
-		
-		else if(action == GLFW.GLFW_PRESS) {	
-			if(key == GLFW.GLFW_KEY_KP_1 || key == GLFW.GLFW_KEY_1) {
-				player.setSlot(0);
-			}
-			
-			else if(key == GLFW.GLFW_KEY_KP_2 || key == GLFW.GLFW_KEY_2) {
-				player.setSlot(1);
-			}
-			
-			else if(key == GLFW.GLFW_KEY_KP_3 || key == GLFW.GLFW_KEY_3) {
-				player.setSlot(2);
-			}
-			
-			else if(key == GLFW.GLFW_KEY_KP_4 || key == GLFW.GLFW_KEY_4) {
-				player.setSlot(3);
-			}
-			
-			else if(key == GLFW.GLFW_KEY_KP_5 || key == GLFW.GLFW_KEY_5) {
-				player.setSlot(4);
-			}
-			
-			else if(key == GLFW.GLFW_KEY_KP_6 || key == GLFW.GLFW_KEY_6) {
-				player.setSlot(5);
-			}
-			
-			else if(key == GLFW.GLFW_KEY_KP_7 || key == GLFW.GLFW_KEY_7) {
-				player.setSlot(6);
-			}
-			
-			else if(key == GLFW.GLFW_KEY_KP_8 || key == GLFW.GLFW_KEY_8) {
-				player.setSlot(7);
-			}
-			
-			else if(key == GLFW.GLFW_KEY_KP_9 || key == GLFW.GLFW_KEY_9) {
-				player.setSlot(8);
-			}
+		if(action == GLFW.GLFW_PRESS) {
+			switch(key) {
+				case Controls.KEYBIND_DOWN:
+					Item item = player.removeSelectedItem();
+					if(item != null) {
+						//TODO deal with entityID for entities created outside server directly
+						ItemEntity entity = new ItemEntity(0, item, player.getPositionX(), player.getPositionY() + player.getHeight());
+						entity.setVelocityX((float) (Math.random() * 0.4f - 0.2f));
+						entity.setVelocityY((float) (Math.random() * 0.3f));
+						world.add(entity);
+						world.getAudioSystem().playSound(Sounds.THROW, player.getPositionX(), player.getPositionY(), entity.getVelocityX(), entity.getVelocityY(), 1.0f, 1f);
+					}
+					break;
+				case GLFW.GLFW_KEY_KP_1:
+				case GLFW.GLFW_KEY_1:
+					player.setSlot(0);
+					break;
+				case GLFW.GLFW_KEY_KP_2:
+				case GLFW.GLFW_KEY_2:
+					player.setSlot(1);
+					break;
+				case GLFW.GLFW_KEY_KP_3:
+				case GLFW.GLFW_KEY_3:
+					player.setSlot(2);
+					break;
+				case GLFW.GLFW_KEY_KP_4:
+				case GLFW.GLFW_KEY_4:
+					player.setSlot(3);
+					break;
+				case GLFW.GLFW_KEY_KP_5:
+				case GLFW.GLFW_KEY_5:
+					player.setSlot(4);
+					break;
+			    case GLFW.GLFW_KEY_KP_6:
+				case GLFW.GLFW_KEY_6:
+					player.setSlot(5);
+					break;
+				case GLFW.GLFW_KEY_KP_7:
+				case GLFW.GLFW_KEY_7:
+					player.setSlot(6);
+					break;
+				case GLFW.GLFW_KEY_KP_8:
+				case GLFW.GLFW_KEY_8:
+					player.setSlot(7);
+					break;
+			    case GLFW.GLFW_KEY_KP_9:
+				case GLFW.GLFW_KEY_9:
+					player.setSlot(8);
+					break;
+			}	
 		}
 	}
 }
