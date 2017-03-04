ce0b15f48337ee9af48a76ebf318139058236da1
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2017-03-03 19:01:06 -0800

    Made light optimizations in BlockGrid and BlockGridUpdater

--- src/ritzow/solomon/engine/world/base/BlockGridUpdater.java
+++ src/ritzow/solomon/engine/world/base/BlockGridUpdater.java
@@ -14,20 +14,22 @@ public final class BlockGridUpdater implements Runnable, Exitable {
 
 	@Override
 	public void run() {
-		try {
-			while(!exit) {
-				for(int row = 0; row < blocks.getHeight(); row++) {
-					for(int column = 0; column < blocks.getWidth(); column++) {
-						if(blocks.isBlock(column, row) && !blocks.isSurrounded(column, row) && !isStable(column, row)) {
-							blocks.destroy(world, column, row);
-						}
+		int height = blocks.getHeight();
+		int width = blocks.getWidth();
+		while(!exit) {
+			for(int row = 0; row < height; row++) {
+				for(int column = 0; column < width; column++) {
+					if(blocks.isBlock(column, row) && !isStable(column, row)) {
+						blocks.destroy(world, column, row);
 					}
-					if(!exit)
-						Thread.sleep(1);
+				}
+				
+				try {
+					Thread.sleep(1); //sleep after each row
+				} catch (InterruptedException e) {
+					System.out.println("Block Grid Updater interrupted");
 				}
 			}
-		} catch (InterruptedException e) {
-			System.out.println("Block Grid Updater interrupted");
 		}
 		
 		synchronized(this) {
