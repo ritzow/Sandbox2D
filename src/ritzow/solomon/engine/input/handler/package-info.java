eddeab8cad2d546136efd64875e771ab046d036b
Solomon Ritzow <solduck@gmail.com>
2017-03-03 19:25:58 -0800

    Removed input handler package info

--- src/ritzow/solomon/engine/input/handler/package-info.java
+++
@@ -1,6 +0,0 @@
-
-/**
- * Contains interfaces that map to GLFW event callbacks, instances of these interfaces can be passed to a Display's InputManager to receive events for that Display
- */
-package ritzow.solomon.engine.input.handler;
-
