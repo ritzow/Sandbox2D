a5714696eefeded2eb6343af0689d3f985c3e0b1
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2017-03-07 19:07:51 -0800

    Updated imports in GraphicsUpdater

--- src/ritzow/solomon/engine/graphics/GraphicsUpdater.java
+++ src/ritzow/solomon/engine/graphics/GraphicsUpdater.java
@@ -1,7 +1,15 @@
 package ritzow.solomon.engine.graphics;
 
 import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_BLEND;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.glBlendFunc;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glViewport;
 
 import java.io.File;
 import java.io.IOException;
