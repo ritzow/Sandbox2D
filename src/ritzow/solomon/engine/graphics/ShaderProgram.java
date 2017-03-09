633fbe1d98bb01bbb7aa1ae82420c3af83b3f955
Solomon Ritzow <solduck@gmail.com>
2017-03-08 22:23:51 -0800

    Updated imports

--- src/ritzow/solomon/engine/graphics/ShaderProgram.java
+++ src/ritzow/solomon/engine/graphics/ShaderProgram.java
@@ -1,18 +1,6 @@
 package ritzow.solomon.engine.graphics;
 
-import static org.lwjgl.opengl.GL20.glAttachShader;
-import static org.lwjgl.opengl.GL20.glBindAttribLocation;
-import static org.lwjgl.opengl.GL20.glCreateProgram;
-import static org.lwjgl.opengl.GL20.glDeleteProgram;
-import static org.lwjgl.opengl.GL20.glDetachShader;
-import static org.lwjgl.opengl.GL20.glGetUniformLocation;
-import static org.lwjgl.opengl.GL20.glLinkProgram;
-import static org.lwjgl.opengl.GL20.glUniform1f;
-import static org.lwjgl.opengl.GL20.glUniform1i;
-import static org.lwjgl.opengl.GL20.glUniform4f;
-import static org.lwjgl.opengl.GL20.glUniformMatrix4fv;
-import static org.lwjgl.opengl.GL20.glUseProgram;
-import static org.lwjgl.opengl.GL20.glValidateProgram;
+import static org.lwjgl.opengl.GL20.*;
 
 class ShaderProgram {
 	public static final int ATTRIBUTE_POSITIONS = 0;
