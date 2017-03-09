6ecbb2982c8c3d7b60d3f40ed80783901010a336
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2017-03-08 22:24:04 -0800

    Created Lobby and Team classes (probably temporary)

---
+++ src/ritzow/solomon/engine/game/Lobby.java
@@ -0,0 +1,8 @@
+package ritzow.solomon.engine.game;
+
+import ritzow.solomon.engine.graphics.Renderable;
+import ritzow.solomon.engine.util.Transportable;
+
+public interface Lobby extends Transportable, Renderable {
+	
+}
