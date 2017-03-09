6b98608964acda7f49bd8f7c24011bf0c6336b4d
Solomon Ritzow <solduck@gmail.com>
2017-03-08 22:24:04 -0800

    Created Lobby and Team classes (probably temporary)

---
+++ src/ritzow/solomon/engine/game/Team.java
@@ -0,0 +1,14 @@
+package ritzow.solomon.engine.game;
+
+public final class Team {
+	protected final String name;
+	protected final int red, green, blue;
+	
+	public Team(String name, int red, int green, int blue) {
+		super();
+		this.name = name;
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+	}
+}
