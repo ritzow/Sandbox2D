57daf4bb49f3e907362a2188423cc79a8e0abc5b
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2017-04-27 18:26:44 -0700

    Updated some World updating stuff

--- src/ritzow/solomon/engine/world/base/BlockGridUpdater.java
+++ src/ritzow/solomon/engine/world/base/BlockGridUpdater.java
@@ -1,14 +1,15 @@
 package ritzow.solomon.engine.world.base;
 
 import ritzow.solomon.engine.util.Exitable;
+import ritzow.solomon.engine.world.block.Block;
 
 public final class BlockGridUpdater implements Runnable, Exitable {
 	private volatile boolean exit, finished;
 	private final BlockGrid blocks;
-	//private final World world;
+	private final World world;
 	
 	public BlockGridUpdater(World world, BlockGrid blocks) {
-		//this.world = world;
+		this.world = world;
 		this.blocks = blocks;
 	}
 
@@ -16,23 +17,30 @@ public final class BlockGridUpdater implements Runnable, Exitable {
 	public void run() {
 		int height = blocks.getHeight();
 		int width = blocks.getWidth();
+		long currentTime;
+		long previousTime = System.nanoTime();
 		while(!exit) {
-			for(int row = 0; row < height; row++) {	
-				for(int column = 0; column < width; column++) {
-					
+			try {
+			    currentTime = System.nanoTime();
+			    float updateTime = (currentTime - previousTime) * 0.0000000625f;
+				for(int row = 0; row < height; row++) {	
+					for(int column = 0; column < width; column++) {
+						Block block = blocks.get(column, row);
+						if(block != null) {
+							block.update(world, updateTime);
+						}
+					}
+					previousTime = currentTime;
 				}
-				
-				try {
-					Thread.sleep(1); //sleep after each row
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
 		
 		synchronized(this) {
 			finished = true;
-			this.notifyAll();
+			notifyAll();
 		}
 	}
 	
