a066138ad5e1bbaf643d688702eedb22e86c7b47
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2018-05-30 01:38:38 -0700

    Added new methods to ClientUtility

--- client/src/ritzow/sandbox/client/util/ClientUtility.java
+++ client/src/ritzow/sandbox/client/util/ClientUtility.java
@@ -3,7 +3,7 @@ package ritzow.sandbox.client.util;
 import ritzow.sandbox.client.graphics.Camera;
 
 public class ClientUtility {
-	public static float mouseHorizontalToWorld(Camera camera, float mouseX, int frameWidth, int frameHeight) {
+	public static float pixelHorizontalToWorld(Camera camera, int mouseX, int frameWidth, int frameHeight) {
 		float worldX = (2f * mouseX) / frameWidth - 1f; //normalize the mouse coordinate
 		worldX /= frameHeight/(float)frameWidth; 		//apply aspect ratio
 		worldX /= camera.getZoom(); 					//apply zoom
@@ -11,10 +11,30 @@ public class ClientUtility {
 		return worldX;
 	}
 	
-	public static float mouseVerticalToWorld(Camera camera, float mouseY, int frameHeight) {
+	public static float pixelVerticalToWorld(Camera camera, int mouseY, int frameWidth, int frameHeight) {
 		float worldY = -((2f * mouseY) / frameHeight - 1f);
 		worldY /= camera.getZoom();
 		worldY += camera.getPositionY();
 		return worldY;
+	}
+	
+	public static float getViewRightBound(Camera camera, int framebufferWidth, int framebufferHeight) {
+		//far right of screen, after accounting for aspect ratio, in world coordinates
+		return framebufferWidth/(camera.getZoom() * framebufferHeight) + camera.getPositionX();
+	}
+	
+	public static float getViewLeftBound(Camera camera, int framebufferWidth, int framebufferHeight) {
+		//far left of screen after accounting for aspect ratio, in world coordinates
+		return -getViewRightBound(camera, framebufferWidth, framebufferHeight);
+	}
+	
+	//width and height for any future need
+	public static float getViewTopBound(Camera camera, int framebufferWidth, int framebufferHeight) {
+		return 1/camera.getZoom() + camera.getPositionY();
+	}
+	
+	//width and height for any future need
+	public static float getViewBottomBound(Camera camera, int framebufferWidth, int framebufferHeight) {
+		return -getViewTopBound(camera, framebufferWidth, framebufferHeight);
 	}
 }
