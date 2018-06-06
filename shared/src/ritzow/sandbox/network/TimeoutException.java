fbac84011ac1855f41071b75c7021f575d35ab30
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2018-06-06 01:42:10 -0700

    Stopped TimeoutException from generating stack traces

--- shared/src/ritzow/sandbox/network/TimeoutException.java
+++ shared/src/ritzow/sandbox/network/TimeoutException.java
@@ -4,22 +4,18 @@ public class TimeoutException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public TimeoutException() {
-		super();
-	}
-
-	public TimeoutException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
+		super(null, null, false, false);
 	}
 
 	public TimeoutException(String message, Throwable cause) {
-		super(message, cause);
+		super(message, cause, false, false);
 	}
 
 	public TimeoutException(String message) {
-		super(message);
+		super(message, null, false, false);
 	}
 
 	public TimeoutException(Throwable cause) {
-		super(cause);
+		super(null, cause, false, false);
 	}
 }
