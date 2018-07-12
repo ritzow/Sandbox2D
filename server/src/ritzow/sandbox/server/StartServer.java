aca0204f620ada1941b7ebd322d04bde0c9a1a3e
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2018-07-12 15:52:57 -0700

    Improved StartServer with a better command parsing system.

--- server/src/ritzow/sandbox/server/StartServer.java
+++ server/src/ritzow/sandbox/server/StartServer.java
@@ -1,15 +1,20 @@
 package ritzow.sandbox.server;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.BindException;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-import ritzow.sandbox.data.ByteUtil;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import ritzow.sandbox.data.Bytes;
 import ritzow.sandbox.network.Protocol;
-import ritzow.sandbox.server.Server.ClientState;
+import ritzow.sandbox.server.network.Server;
+import ritzow.sandbox.server.network.Server.ClientState;
 import ritzow.sandbox.util.Utility;
 import ritzow.sandbox.world.World;
 import ritzow.sandbox.world.block.DirtBlock;
@@ -17,98 +22,49 @@ import ritzow.sandbox.world.block.GrassBlock;
 import ritzow.sandbox.world.entity.PlayerEntity;
 
 public final class StartServer {
-	private static volatile boolean exit, save;
+	private static volatile boolean save = true;
 	
-	public static void main(String... args) {
-		
+	public static void main(String... args) throws IOException {
 		try {
 			Thread.currentThread().setName("Update and Process");
 			Server server = Server.open(new InetSocketAddress(Protocol.DEFAULT_SERVER_UDP_PORT));
 
 			//the save file to try to load the world from
-			Path saveFile = Paths.get(args.length > 0 ? args[0] : "data/worlds/world.dat");
+			Path saveFile = Path.of(args.length > 0 ? args[0] : "data/worlds/world.dat");
 			
 			//if a save file exists, load it, otherwise generate a world
-			World world = Files.exists(saveFile) ? 
-					loadWorld(saveFile) : generateWorld(100, 100);
+			World world = Files.exists(saveFile) ? loadWorld(saveFile) : generateWorld(100, 100);
 			server.start(world);
 			
 			//read user input commands
 			System.out.println("Startup Complete.");
-			System.out.println("Type 'exit' to stop server or 'list' to list connected clients");
-			new Thread(() -> runConsoleReader(server), "Console Input").start();
+			new Thread(() -> runCommandParser(server, System.in)).start();
 			
-			while(!exit) {
+			while(server.isRunning()) {
 				server.update();
 				Utility.sleep(1);
 			}
-			server.stop();
 			
-			//save world to file if enabled
-			if(save) {
-				saveWorld(world, saveFile);
-			} else {
-				System.out.println("Server stopped.");
-			}
+			if(save) saveWorld(world, saveFile);
 		} catch(BindException e) {
 			System.out.println("Could not start server: '" + e.getMessage() + "'");
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 	
-	private static void runConsoleReader(Server server) {
-		try(Scanner scanner = new Scanner(System.in)) {
-			String next;
-			reader: while(true) {
-				switch(next = scanner.nextLine()) {
-				case "exit":
-				case "quit":
-				case "stop":
-					exit = true;
-					save = true;
-					break reader;
-				case "abort":
-					exit = true;
-					save = false;
-					break reader;
-				case "list":
-					if(server.getConnectedClients() == 0) {
-						System.out.println("No connected clients.");
-					} else {
-						System.out.println("Connected clients:");
-						for(ClientState client : server.listClients()) {
-							System.out.print("\t - ");
-							System.out.println(client);
-						}
-					}
-					break;
-				case "disconnect":
-					server.disconnectAll("server manual disconnect");
-					break;
-				default:
-					server.broadcastConsoleMessage(next);
-					System.out.println("Sent message '" + next + "' to " + 
-					server.getConnectedClients() + " connected client(s).");
-				}
-			}
-		}
+	public static World loadWorld(Path file) throws IOException {
+		return SerializationProvider.getProvider().deserialize(Bytes.decompress(Files.readAllBytes(file)));
 	}
 	
 	public static void saveWorld(World world, Path file) {
 		try {
 			System.out.print("Saving world... ");
 			world.removeIf(e -> e instanceof PlayerEntity); //remove players before saving to file
-			byte[] serialized = ByteUtil.compress(SerializationProvider.getProvider().serialize(world));
+			byte[] serialized = Bytes.compress(SerializationProvider.getProvider().serialize(world));
 			Files.write(file, serialized);
 			System.out.println("world saved to " + Utility.formatSize(serialized.length) + ".");
 		} catch (IOException e) {
 			System.out.println("Error while saving world to file: " + e.getMessage());
 		}
-	}
-	
-	public static World loadWorld(Path file) throws IOException {
-		return SerializationProvider.getProvider().deserialize(ByteUtil.decompress(Files.readAllBytes(file)));
 	}
 	
 	public static World generateWorld(int width, int height) {
@@ -124,5 +80,66 @@ public final class StartServer {
 			world.getBackground().set(column, (int)halfheight, new DirtBlock());
 		}
 		return world;
+	}
+	
+	private static void registerCommands(Server server) {
+		register("stop", StartServer::stopCommand);
+		register("abort", StartServer::abortCommand);
+		register("list", StartServer::listCommmand);
+		register("disconnect", StartServer::disconnectCommand);
+		register("say", StartServer::sayCommand);
+	}
+	
+	private static void stopCommand(Server server, String args) {
+		server.stop();
+	}
+
+	private static void sayCommand(Server server, String args) {
+		String message = String.join(" ", args);
+		server.broadcastConsoleMessage(message);
+		System.out.println("Message '" + message + "' sent to " + server.getConnectedClients() + " client(s).");
+	}
+	
+	private static void abortCommand(Server server, String args) {
+		save = false;
+		server.stop();
+		System.out.println("Server stopped without saving to file.");
+	}
+	
+	private static void listCommmand(Server server, String args) {
+		if(server.getConnectedClients() == 0) {
+			System.out.println("No connected clients.");
+		} else {
+			System.out.println("Connected clients:");
+			for(ClientState client : server.listClients()) {
+				System.out.print("\t - ");
+				System.out.println(client);
+			}
+		}
+	}
+	
+	private static void disconnectCommand(Server server, String args) {
+		System.out.println("Disconencted all " + server.disconnectAll(String.join(" ", args)) + " connected clients.");
+	}
+	
+	private static Map<String, BiConsumer<Server, String>> commands = new HashMap<>();
+	
+	private static void register(String name, BiConsumer<Server, String> action) {
+		if(commands.putIfAbsent(name, action) != null)
+			throw new IllegalArgumentException(name + " already registered");	
+	}
+	
+	private static void runCommandParser(Server server, InputStream in) {
+		registerCommands(server);
+		System.out.println("Enter commands (" + commands.keySet().stream().collect(Collectors.joining(", ")) + "): ");
+		try(Scanner scan = new Scanner(in)) {
+			while(server.isRunning()) {
+				commands.getOrDefault(scan.next(), StartServer::unknownCommand).accept(server, scan.nextLine().trim());
+			}
+		}
+	}
+	
+	private static void unknownCommand(Server server, String args) {
+		System.out.println("Unknown command.");
 	}
 }
