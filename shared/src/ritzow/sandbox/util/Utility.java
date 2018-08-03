5a4ba189234c800018fc9c88e632effb25066b25
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2018-08-02 20:24:36 -0700

    Improved Utility.updateWorld readability and fixed a bug

--- shared/src/ritzow/sandbox/util/Utility.java
+++ shared/src/ritzow/sandbox/util/Utility.java
@@ -16,20 +16,16 @@ import java.util.stream.Stream;
 import ritzow.sandbox.world.BlockGrid;
 import ritzow.sandbox.world.World;
 
-/**
- * Contains a number of static utility methods relating to various systems (matrix/random math, time, synchronization, hitboxes)
- * @author Solomon Ritzow
- *
- */
+/** Contains a number of static utility methods relating to various systems (matrix/random math, time, synchronization, hitboxes) */
 public final class Utility {
 	private Utility() {
 		throw new UnsupportedOperationException("Utility class cannot be instantiated");
 	}
-	
+
 	public static String formatAddress(InetSocketAddress address) {
 		return "[" + address.getAddress().getHostAddress() + "]:" + (address.getPort() == 0 ? "any" : address.getPort());
 	}
-	
+
 	public static ProtocolFamily getProtocolFamily(InetAddress address) {
 		Class<? extends InetAddress> addressClass = address.getClass();
 		if(addressClass.equals(Inet6Address.class)) {
@@ -40,12 +36,12 @@ public final class Utility {
 			throw new IllegalArgumentException("InetAddress of unknown protocol");
 		}
 	}
-	
+
 	public static InetSocketAddress getAddressOrDefault(String[] args, int index, InetAddress defaultAddress, int defaultPort) throws NumberFormatException, UnknownHostException {
-		return new InetSocketAddress(args.length > index ? InetAddress.getByName(args[index]) : defaultAddress, 
+		return new InetSocketAddress(args.length > index ? InetAddress.getByName(args[index]) : defaultAddress,
 				args.length > 1 ? Integer.parseInt(args[index + 1]) : defaultPort);
 	}
-	
+
 	public static <T extends InetAddress> T getPublicAddress(Class<T> addressType) throws SocketException {
 		Set<InetAddress> addresses = NetworkInterface.networkInterfaces()
 			.filter(network -> {
@@ -64,33 +60,33 @@ public final class Utility {
 			throw new RuntimeException("No address of type " + addressType + " available");
 		return addressType.cast(addresses.iterator().next());
 	}
-	
+
 	public static int clampLowerBound(int min, float value) {
 		return Math.max(min, (int)Math.floor(value));
 	}
-	
+
 	public static int clampUpperBound(int max, float value) {
 		return Math.min(max, (int)Math.ceil(value));
 	}
-	
+
 	public interface GridAction {
 		void perform(int x, int y);
 	}
-	
+
 	public static void forEachBlock(BlockGrid grid, float leftX, float rightX, float bottomY, float topY, GridAction action) {
 		//calculate block grid bounds TODO fix after adding chunk system, allow for negatives
 		int leftBound = 	clampLowerBound(0, leftX);
 		int rightBound = 	clampUpperBound(grid.getWidth()-1, rightX);
 		int topBound = 		clampUpperBound(grid.getHeight()-1, topY);
 		int bottomBound = 	clampLowerBound(0, bottomY);
-		
+
 		for(int row = bottomBound; row <= topBound; row++) {
 			for(int column = leftBound; column <= rightBound; column++) {
 				action.perform(column, row);
 			}
 		}
 	}
-	
+
 	/**
 	 * Sleeps for the provided number of milliseconds
 	 * @param milliseconds the number of milliseconds to sleep for
@@ -103,11 +99,13 @@ public final class Utility {
 			throwInterrupted(e);
 		}
 	}
-	
+
 	private static void throwInterrupted(InterruptedException cause) {
 		throw new IllegalStateException(Thread.currentThread() + " should not be interrupted", cause);
 	}
-	
+
+	private static final float UPDATE_TIME_MAX_ALLOWED = 100;
+
 	/**
 	 * Updates the world
 	 * @param world the world to update
@@ -117,26 +115,27 @@ public final class Utility {
 	 * @return the start time for the world update (should replace the previous time)
 	 */
 	public static long updateWorld(World world, long previousTime, float maxTimestep, float timeScale) {
-		if(previousTime == 0)
-			throw new IllegalArgumentException("previousTime is 0");
-		long current = System.nanoTime(); //get the current time
-		float totalUpdateTime = (current - previousTime) / timeScale;
-		
+		long start = System.nanoTime(); //get the current time
+		float updateTimeRemaining = (start - previousTime) / timeScale;
+		if(updateTimeRemaining > UPDATE_TIME_MAX_ALLOWED)
+			throw new IllegalArgumentException(updateTimeRemaining + " is greater than the max allowed world update time");
+
 		//update the world with a timestep of at most maxTimestep until the world is up to date.
-		for(float time = totalUpdateTime; totalUpdateTime > 0; totalUpdateTime -= time) {
-			time = Math.min(totalUpdateTime, maxTimestep);
-			world.update(time);
-			totalUpdateTime -= time;
+		while(updateTimeRemaining > 0) {
+			float singleUpdateTime = Math.min(updateTimeRemaining, maxTimestep);
+			world.update(singleUpdateTime);
+			updateTimeRemaining -= singleUpdateTime;
 		}
-		return current;
+
+		return start;
 	}
-	
+
 	public static void notify(Object o) {
 		synchronized(o) {
 			o.notifyAll();
 		}
 	}
-	
+
 	/**
 	 * Waits on {@code lock} and returns once {@code condition} returns {@code true}.
 	 * @param lock the object to wait to be notified by.
@@ -151,7 +150,7 @@ public final class Utility {
 			}
 		}
 	}
-	
+
 	public static void waitOnCondition(Object lock, long timeoutMillis, BooleanSupplier condition) {
 		if(timeoutMillis == 0)
 			throw new IllegalArgumentException("timeout of 0 not allowed, use other overload");
@@ -168,32 +167,32 @@ public final class Utility {
 				throw new IllegalStateException("condition not met");
 		}
 	}
-	
+
 	public static float convertRange(float oldMin, float oldMax, float newMin, float newMax, float value) {
 		return (((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin;
 	}
-	
+
 	/** Returns an rotation in radians that represents the change in angle after rotating around a point **/
 	public static float rotateAround(float x, float y, float centerX, float centerY) {
 		return (float)Math.atan2(x-centerX, y-centerY);
 	}
-	
+
 	public static float rotateCoordX(float x, float y, float centerX, float centerY, float angle) {
 		return (float)((x - centerX)*Math.cos(angle) - (y - centerY)*Math.sin(angle)) + x;
 	}
-	
+
 	public static float rotateCoordY(float x, float y, float centerX, float centerY, float angle) {
 		return (float)((y - centerY)*Math.cos(angle) + (x - centerX)*Math.sin(angle)) + y;
 	}
-	
+
 	public static void printUsedMemory() {
 		System.out.println(formatSize(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
 	}
-	
+
 	public static void printTimeSince(long nanoseconds) {
 		System.out.println(formatTime(System.nanoTime() - nanoseconds));
 	}
-	
+
 	public static String formatSize(long bytes) {
 		String units; double value;
 		if(bytes < 1000) {
@@ -211,7 +210,7 @@ public final class Utility {
 		}
 		return formatNumber(value, 2) + " " + units;
 	}
-	
+
 	public static String formatTime(long nanoseconds) {
 		String units; double value;
 		if(nanoseconds < 1000) {
@@ -229,34 +228,34 @@ public final class Utility {
 		}
 		return formatNumber(value, 2) + " " + units;
 	}
-	
+
 	public static String formatNumber(double value, int decimals) {
 		long asInteger = Math.round(value);
 		String number = Double.toString(value);
-		return asInteger == value ? Long.toString(asInteger) : (decimals > 0 ? number.substring(0, Math.min(number.length(), 
+		return asInteger == value ? Long.toString(asInteger) : (decimals > 0 ? number.substring(0, Math.min(number.length(),
 			number.indexOf('.') + decimals)) : ("~" + asInteger));
 	}
-	
+
 	public static double millisBetween(long startNanos, long endNanos) {
 		return nanosToMillis(endNanos - startNanos);
 	}
-	
+
 	public static long millisToNanos(long milliseconds) {
 		return milliseconds * 1_000_000;
 	}
-	
+
 	public static double nanosToMillis(long nanoseconds) {
 		return nanoseconds / 1_000_000D;
 	}
-	
+
 	public static long nanosSince(long nanosStart) {
 		return System.nanoTime() - nanosStart;
 	}
-	
+
 	public static double millisSince(long nanosStart) {
 		return nanosToMillis(nanosSince(nanosStart));
 	}
-	
+
 	public static float addMagnitude(float number, float magnitude) {
 		if(number < 0)
 			return Math.min(0, number - magnitude);
@@ -265,43 +264,43 @@ public final class Utility {
 		else
 			return 0;
 	}
-	
+
 	public static float randomFloat(float min, float max) {
 		return (float)(Math.random() * (max - min) + min);
 	}
-	
+
 	public static long randomLong(long min, long max) {
 		return (long)(Math.random() * (max - min) + min);
 	}
-	
+
 	public static boolean intersection(float rectangleX, float rectangleY, float width, float height, float pointX, float pointY) {
 		return (pointX <= rectangleX + width/2 && pointX >= rectangleX - width/2) && (pointY <= rectangleY + height/2 && pointY >= rectangleY - height/2);
 	}
-	
+
 	public static boolean intersection(float x, float y, float width, float height, float x2, float y2, float width2, float height2) {
 		return (Math.abs(x - x2) * 2 < (width + width2)) && (Math.abs(y - y2) * 2 < (height + height2));
 	}
-	
+
 	public static float average(float val1, float val2) {
 		return (val1 + val1)/2;
 	}
-	
+
 	public static double distance(double x1, double y1, double x2, double y2) {
 		return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
 	}
-	
+
 	public static float distance(float x1, float y1, float x2, float y2) {
 		return (float)Math.sqrt(distanceSquared(x1, y1, x2, y2));
 	}
-	
+
 	public static boolean withinDistance(float x1, float y1, float x2, float y2, float distance) {
 		return distanceSquared(x1, y1, x2, y2) <= distance*distance;
 	}
-	
+
 	private static float distanceSquared(float x1, float y1, float x2, float y2) {
 		return (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
 	}
-	
+
 	/** Returns the maximum value of the component opposite the one passed in (x if y given) within a given radius **/
 	public static float maxComponentInRadius(float otherComp, float radius) {
 		return (float)Math.sqrt(radius * radius - otherComp * otherComp);
