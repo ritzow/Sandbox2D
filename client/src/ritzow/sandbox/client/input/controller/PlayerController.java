7108181b2ca5b806e504f0531e70b3db16a6d0da
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2019-05-27 23:41:11 -0700

    Updated PlayerController to use new switch statement

--- client/src/ritzow/sandbox/client/input/controller/PlayerController.java
+++ client/src/ritzow/sandbox/client/input/controller/PlayerController.java
@@ -1,12 +1,13 @@
 package ritzow.sandbox.client.input.controller;
 
-import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_REPEAT;
+import static ritzow.sandbox.network.Protocol.PlayerAction.*;
 
 import ritzow.sandbox.client.input.ControlScheme;
 import ritzow.sandbox.client.input.EventDelegator;
 import ritzow.sandbox.client.input.handler.KeyHandler;
 import ritzow.sandbox.client.network.GameTalker;
-import ritzow.sandbox.network.Protocol.PlayerAction;
 
 public class PlayerController implements KeyHandler {
 	protected final GameTalker client;
@@ -26,20 +27,16 @@ public class PlayerController implements KeyHandler {
 	@Override
 	public void keyboardButton(int key, int scancode, int action, int mods) {
 		if(action != GLFW_REPEAT) {
-			switch(key) {
-			case ControlScheme.KEYBIND_UP:
-				client.sendPlayerAction(action == GLFW_PRESS ? PlayerAction.MOVE_UP_START : PlayerAction.MOVE_UP_STOP);
-				break;
-			case ControlScheme.KEYBIND_RIGHT:
-				client.sendPlayerAction(action == GLFW_PRESS ? PlayerAction.MOVE_RIGHT_START : PlayerAction.MOVE_RIGHT_STOP);
-				break;
-			case ControlScheme.KEYBIND_LEFT:
-				client.sendPlayerAction(action == GLFW_PRESS ? PlayerAction.MOVE_LEFT_START : PlayerAction.MOVE_LEFT_STOP);
-				break;
-			case ControlScheme.KEYBIND_DOWN:
-				client.sendPlayerAction(action == GLFW_PRESS ? PlayerAction.MOVE_DOWN_START : PlayerAction.MOVE_DOWN_STOP);
-				break;
-			}
+			var send = switch(key) {
+				case ControlScheme.KEYBIND_UP 	-> action == GLFW_PRESS ? MOVE_UP_START : MOVE_UP_STOP;
+				case ControlScheme.KEYBIND_RIGHT -> action == GLFW_PRESS ? MOVE_RIGHT_START : MOVE_RIGHT_STOP;
+				case ControlScheme.KEYBIND_LEFT -> action == GLFW_PRESS ? MOVE_LEFT_START : MOVE_LEFT_STOP;
+				case ControlScheme.KEYBIND_DOWN -> action == GLFW_PRESS ? MOVE_DOWN_START : MOVE_DOWN_STOP;
+				default -> null;
+			};
+			
+			if(send != null)
+				client.sendPlayerAction(send);
 		}
 	}
 }
