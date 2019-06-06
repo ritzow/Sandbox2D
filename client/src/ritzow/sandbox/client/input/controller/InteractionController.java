b6350fb91d76c63f3526d08a2cfaa99206291278
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2019-06-06 10:41:21 -0700

    Updated InteractionController with new switch statement

--- client/src/ritzow/sandbox/client/input/controller/InteractionController.java
+++ client/src/ritzow/sandbox/client/input/controller/InteractionController.java
@@ -71,8 +71,8 @@ public final class InteractionController implements MouseButtonHandler, CursorPo
 
 	@Override
 	public void mouseButton(int button, int action, int mods) {
-		primaryAction = ((button == GLFW_MOUSE_BUTTON_LEFT || primaryAction) && action == GLFW_PRESS);
-		secondaryAction = ((button == GLFW_MOUSE_BUTTON_RIGHT || secondaryAction) && action == GLFW_PRESS);
+		primaryAction = action == GLFW_PRESS && (button == GLFW_MOUSE_BUTTON_LEFT || primaryAction);
+		secondaryAction = action == GLFW_PRESS && (button == GLFW_MOUSE_BUTTON_RIGHT || secondaryAction);
 	}
 
 	@Override
@@ -81,51 +81,21 @@ public final class InteractionController implements MouseButtonHandler, CursorPo
 		mouseY = (int) ypos;
 	}
 
+	//TODO using callbacks is stupid in singlethreaded, check keyboard buttons in update instead.
 	@Override
-	public void keyboardButton(int key, int scancode, int action, int mods) { //TODO using callbacks is stupid in singlethreaded, check keyboard buttons in update instead.
-		try {
-			if(action == GLFW_PRESS) {
-				switch(key) {
-					case GLFW_KEY_KP_1:
-					case GLFW_KEY_1:
-						client.getPlayer().setSlot(0);
-						break;
-					case GLFW_KEY_KP_2:
-					case GLFW_KEY_2:
-						client.getPlayer().setSlot(1);
-						break;
-					case GLFW_KEY_KP_3:
-					case GLFW_KEY_3:
-						client.getPlayer().setSlot(2);
-						break;
-					case GLFW_KEY_KP_4:
-					case GLFW_KEY_4:
-						client.getPlayer().setSlot(3);
-						break;
-					case GLFW_KEY_KP_5:
-					case GLFW_KEY_5:
-						client.getPlayer().setSlot(4);
-						break;
-				    case GLFW_KEY_KP_6:
-					case GLFW_KEY_6:
-						client.getPlayer().setSlot(5);
-						break;
-					case GLFW_KEY_KP_7:
-					case GLFW_KEY_7:
-						client.getPlayer().setSlot(6);
-						break;
-					case GLFW_KEY_KP_8:
-					case GLFW_KEY_8:
-						client.getPlayer().setSlot(7);
-						break;
-				    case GLFW_KEY_KP_9:
-					case GLFW_KEY_9:
-						client.getPlayer().setSlot(8);
-						break;
-				}
+	public void keyboardButton(int key, int scancode, int action, int mods) {
+		if(action == GLFW_PRESS) {
+			switch(key) {
+				case GLFW_KEY_1, GLFW_KEY_KP_1 -> client.getPlayer().setSlot(0);
+				case GLFW_KEY_2, GLFW_KEY_KP_2 -> client.getPlayer().setSlot(1);
+				case GLFW_KEY_3, GLFW_KEY_KP_3 -> client.getPlayer().setSlot(2);
+				case GLFW_KEY_4, GLFW_KEY_KP_4 -> client.getPlayer().setSlot(3);
+				case GLFW_KEY_5, GLFW_KEY_KP_5 -> client.getPlayer().setSlot(4);
+			    case GLFW_KEY_6, GLFW_KEY_KP_6 -> client.getPlayer().setSlot(5);
+				case GLFW_KEY_7, GLFW_KEY_KP_7 -> client.getPlayer().setSlot(6);
+				case GLFW_KEY_8, GLFW_KEY_KP_8 -> client.getPlayer().setSlot(7);
+			    case GLFW_KEY_9, GLFW_KEY_KP_9 -> client.getPlayer().setSlot(8);
 			}
-		} catch(InterruptedException e) {
-			e.printStackTrace();
 		}
 	}
 }
