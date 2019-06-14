3275b89a7d943786aa19ebec92fa41201d78f88b
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2019-06-14 07:24:25 -0700

    Simplified RenderManager

--- client/src/ritzow/sandbox/client/graphics/RenderManager.java
+++ client/src/ritzow/sandbox/client/graphics/RenderManager.java
@@ -3,29 +3,13 @@ package ritzow.sandbox.client.graphics;
 import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
 import static org.lwjgl.opengl.GL46C.*;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import org.lwjgl.glfw.GLFW;
-import ritzow.sandbox.client.input.EventDelegator;
-import ritzow.sandbox.client.input.handler.FramebufferSizeHandler;
 
-/** Contains instance methods for initializing and destroying OpenGL, and rendering and updating the display**/
-public final class RenderManager implements FramebufferSizeHandler {
-	private boolean updateViewport;
-	private int framebufferWidth, framebufferHeight;
-	private final List<Renderer> renderers;
-	private final Framebuffer displayBuffer;
+/** Contains methods for initializing and destroying the OpenGL context and rendering and updating the display **/
+public class RenderManager {
+	private static final Framebuffer displayBuffer = new Framebuffer(0);
 
-	public RenderManager(Display display) {
-		this.link(display.getEventDelegator());
-		this.renderers = new ArrayList<>();
-		this.displayBuffer = new Framebuffer(0);
-	}
-
-	@SuppressWarnings("static-method")
-	public void initialize(Display display) {
-		display.setGraphicsContextOnThread();
+	public static void initializeContext() {
 		org.lwjgl.opengl.GL.createCapabilities();
 		glfwSwapInterval(0);
 		glDisable(GL_DEPTH_TEST);
@@ -33,46 +17,18 @@ public final class RenderManager implements FramebufferSizeHandler {
 		GraphicsUtility.checkErrors();
 	}
 
-	public void close(Display display) {
+	public static void closeContext() {
 		GraphicsUtility.checkErrors();
 		GLFW.glfwMakeContextCurrent(0);
 		org.lwjgl.opengl.GL.destroy();
-		unlink(display.getEventDelegator());
 	}
 
-	public void run(Display display) {
-		int width = framebufferWidth, height = framebufferHeight;
-		if(width > 0 && height > 0) {
-			if(updateViewport) {
-				glViewport(0, 0, width, height);
-				updateViewport = false;
-			}
-			
-			for(Renderer r : renderers) {
-				r.render(displayBuffer, width, height);
-			}
-
-			display.refresh();
-			GraphicsUtility.checkErrors();
-		}
-	}
-
-	public Collection<Renderer> getRenderers() {
-		return renderers;
-	}
-
-	@Override
-	public void framebufferSize(int width, int height) {
-		framebufferWidth = width;
-		framebufferHeight = height;
-		updateViewport = true;
-	}
-
-	public void link(EventDelegator manager) {
-		manager.framebufferSizeHandlers().add(this);
-	}
-
-	public void unlink(EventDelegator manager) {
-		manager.framebufferSizeHandlers().remove(this);
+	public static void run(Display display, Renderer renderer) {
+		var size = display.getSize();
+		int width = size.x, height = size.y;
+		glViewport(0, 0, width, height);
+		renderer.render(displayBuffer, width, height);
+		display.refresh();
+		GraphicsUtility.checkErrors();
 	}
 }
