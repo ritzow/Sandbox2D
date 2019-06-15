b121e80f93a3677e74c380137f6690de07cedc6f
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2019-06-14 19:06:22 -0700

    Improved prepare-build

--- clientlauncher/Windows/prepare-build.bat
+++ clientlauncher/Windows/prepare-build.bat
@@ -1,29 +1,50 @@
+set "output=x64\Release\Output"
 
 ::clean up previous version
 rmdir /S /Q "include"
-rmdir /S /Q "x64\Release\jvm"
+rmdir /S /Q "%output%"
+rmdir /S /Q "x64\Development\Output"
+mkdir "%output%"
 
-::create a java runtime
+::create the java runtime
 "C:\Program Files\Java\jdk-12\bin\jlink.exe" ^
-	--compress=2 ^
+	--compress=1 ^
 	--no-man-pages ^
 	--strip-debug ^
+	--endian little ^
 	--add-modules java.base,jdk.unsupported ^
-	--output "x64/Release/jvm"
+	--output "%output%\jvm"
+
+::delete unecessary files kept by jlink
+set "jvmbin=%output%\jvm\bin"
+del "%jvmbin%\java.exe"
+del "%jvmbin%\javaw.exe"
+del "%jvmbin%\keytool.exe"
+del "%output%\jvm\lib\jvm.lib"
 
 ::copy files required to compile
-move "x64\Release\jvm\include" "include"
+move "%output%\jvm\include" "include"
 copy "include\win32" "include"
 rmdir /S /Q "include\win32"
 
-::TODO call Build.java and move the resulting files?
-::copy required runtime code and resources
-xcopy /Y "..\..\client\libraries\lwjgl\lwjgl.jar" "x64\Release\"
-xcopy /Y "..\..\client\libraries\lwjgl\lwjgl-opengl.jar" "x64\Release\"
-xcopy /Y "..\..\client\libraries\lwjgl\lwjgl-openal.jar" "x64\Release\"
-xcopy /Y "..\..\client\libraries\lwjgl\lwjgl-glfw.jar" "x64\Release\"
-xcopy /Y "..\..\client\libraries\PNGDecoder\PNGDecoder.jar" "x64\Release\"
-xcopy /E /Y "..\..\client\resources" "x64\Release\resources\"
-xcopy /E /Y "..\..\client\bin" "x64\Release\client\"
-xcopy /E /Y "..\..\shared\bin" "x64\Release\shared\"
+set "client=..\..\client"
+set "shared=..\..\shared"
+set "lwjgl=%client%\libraries\lwjgl"
+
+::copy code, libraries, and resources to output TODO call Build.java and move the resulting files instead of relying on Eclipse IDE build
+xcopy /E /Y /Q "%client%\bin" "%output%\client\"
+xcopy /E /Y /Q "%shared%\bin" "%output%\shared\"
+xcopy /E /Y /Q "%client%\resources" "%output%\resources\"
+xcopy /Y "%lwjgl%\lwjgl.jar" "%output%\"
+xcopy /Y "%lwjgl%\lwjgl-opengl.jar" "%output%\"
+xcopy /Y "%lwjgl%\lwjgl-openal.jar" "%output%\"
+xcopy /Y "%lwjgl%\lwjgl-glfw.jar" "%output%\"
+xcopy /Y "%client%\libraries\PNGDecoder\PNGDecoder.jar" "%output%\"
+
+::copy natives
+xcopy /E /Y "natives" "%output%\"
+
+::clone for development Build
+xcopy /E /Y /Q "x64\Release" "x64\Development\"
+
 PAUSE
