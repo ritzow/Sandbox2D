0414c957e618df1df2f8ec569a0f766a79e3119a
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2019-07-19 14:32:06 -0700

    Added new client-side onBreak and onPlace methods for blocks
    Now uses camera zoom level to adjust volume

--- client/src/ritzow/sandbox/client/world/block/ClientBlockProperties.java
+++ client/src/ritzow/sandbox/client/world/block/ClientBlockProperties.java
@@ -1,9 +1,9 @@
 package ritzow.sandbox.client.world.block;
 
-import static ritzow.sandbox.util.Utility.randomFloat;
-
 import ritzow.sandbox.client.audio.DefaultAudioSystem;
 import ritzow.sandbox.client.audio.Sound;
+import ritzow.sandbox.client.graphics.Camera;
+import ritzow.sandbox.util.Utility;
 import ritzow.sandbox.world.BlockGrid;
 import ritzow.sandbox.world.World;
 import ritzow.sandbox.world.block.Block;
@@ -13,12 +13,21 @@ public interface ClientBlockProperties extends Block {
 	boolean isTransparent();
 	
 	public default void onBreak(World world, BlockGrid grid, float x, float y) {
-		DefaultAudioSystem.getDefault()
-			.playSound(Sound.BLOCK_BREAK.code(), x, y, 0, 0, randomFloat(0.75f, 1.5f), randomFloat(0.75f, 1.5f));
+		throw new UnsupportedOperationException("client does not support default onBreak");
 	}
 
 	public default void onPlace(World world, BlockGrid grid, float x, float y) {
+		throw new UnsupportedOperationException("client does not support default onPlace");
+	}
+	
+	public default void onBreak(World world, BlockGrid grid, Camera camera, float x, float y) {
 		DefaultAudioSystem.getDefault()
-			.playSound(Sound.BLOCK_PLACE.code(), x, y, 0, 0, 1, randomFloat(0.9f, 1.1f));
+		.playSound(Sound.BLOCK_BREAK.code(), x, y, 0, 0, 
+				camera.getZoom() * 2 * Utility.random(0.75f, 1.5f), Utility.random(0.75f, 1.5f));
+	}
+	
+	public default void onPlace(World world, BlockGrid grid, Camera camera, float x, float y) {
+		DefaultAudioSystem.getDefault()
+			.playSound(Sound.BLOCK_PLACE.code(), x, y, 0, 0, camera.getZoom() * 2, Utility.random(0.9f, 1.1f));
 	}
 }
