739280ecdcb497c0d6a85303c74440e7eb5526ab
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2020-02-23 23:33:28 -0800

    Removed comments and unused code from ModelRenderProgram

--- client/src/ritzow/sandbox/client/graphics/ModelRenderProgram.java
+++ client/src/ritzow/sandbox/client/graphics/ModelRenderProgram.java
@@ -24,13 +24,6 @@ public final class ModelRenderProgram extends ShaderProgram {
 			0, 0, 0, 0,
 			0, 0, 0, 1,
 	};
-//
-//	private final float[] cameraMatrix = {
-//			1, 0, 0, 0,
-//			0, 1, 0, 0,
-//			0, 0, 0, 0,
-//			0, 0, 0, 1,
-//	};
 
 	private final float[] viewMatrix = {
 			0, 0, 0, 0,
@@ -38,21 +31,6 @@ public final class ModelRenderProgram extends ShaderProgram {
 			0, 0, 0, 0,
 			0, 0, 0, 1,
 	};
-	
-//	private final float[] transformationMatrix = {
-//			1, 0, 0, 0,
-//			0, 1, 0, 0,
-//			0, 0, 0, 0,
-//			0, 0, 0, 1,
-//	};
-	
-//	private static final int
-//		SCALE_X = 0,
-//		SHEAR_X = 1,
-//		TRANS_X = 3,
-//		SHEAR_Y = 4,
-//		SCALE_Y = 5,
-//		TRANS_Y = 7;
 	
 	private final int
 		uniform_transform,
@@ -78,8 +56,8 @@ public final class ModelRenderProgram extends ShaderProgram {
 		this.uniform_view = getUniformLocation("view");
 		setInteger(getUniformLocation("atlasSampler"), ATLAS_TEXTURE_UNIT);
 		this.atlasTexture = textureAtlas;
-		this.modelProperties = new HashMap<>();
-		this.renderQueue = new ArrayDeque<>();
+		this.modelProperties = new HashMap<Integer, ModelAttributes>();
+		this.renderQueue = new ArrayDeque<RenderInstance>();
 		this.vaoID = register(models);
 	}
 	
@@ -146,13 +124,6 @@ public final class ModelRenderProgram extends ShaderProgram {
 		aspectMatrix[0] = framebufferHeight/(float)framebufferWidth;
 		setMatrix(uniform_view, aspectMatrix);
 	}
-
-//	public void loadViewMatrix2(Camera camera, int framebufferWidth, int framebufferHeight) {
-//		computeAspectMatrix(framebufferWidth, framebufferHeight);
-//		computeCameraMatrix(camera);
-//		multiply(aspectMatrix, cameraMatrix, viewMatrix);
-//		setMatrix(uniform_view, viewMatrix);
-//	}
 	
 	public void loadViewMatrix(Camera camera, int framebufferWidth, int framebufferHeight) {
 		float ratio = framebufferHeight/(float)framebufferWidth;
@@ -166,45 +137,6 @@ public final class ModelRenderProgram extends ShaderProgram {
 		setMatrix(uniform_view, viewMatrix);
 	}
 
-//	//TODO figure out the weird vertical squashing from aspect ratio
-//	private void computeAspectMatrix(int framebufferWidth, int framebufferHeight) {
-//		float ratio = framebufferWidth/(float)framebufferHeight;
-//		aspectMatrix[SCALE_X] = 1/ratio;
-		//float scaleY = 1/ratio;
-		//float scaleX = scaleY/ratio;
-//		aspectMatrix[SCALE_X] = scaleX;
-//		aspectMatrix[SCALE_Y] = scaleY;
-
-//		if(framebufferWidth > framebufferHeight) {
-//			aspectMatrix[SCALE_X] = 1;
-//			aspectMatrix[SCALE_Y] = ratio;
-//		} else {
-//			aspectMatrix[SCALE_X] = 1/ratio;
-//			aspectMatrix[SCALE_Y] = 1;
-//		}
-//	}
-
-//	private void computeCameraMatrix(Camera camera) {
-//		cameraMatrix[SCALE_X] = camera.getZoom();
-//		cameraMatrix[TRANS_X] = -camera.getPositionX() * camera.getZoom();
-//		cameraMatrix[SCALE_Y] = camera.getZoom();
-//		cameraMatrix[TRANS_Y] = -camera.getPositionY() * camera.getZoom();
-//	}
-
-//	//matrix operations on 4x4 1-dimensional arrays
-//	private static void multiply(float[] a, float[] b, float[] destination) {
-//		for(int i = 0; i <= 12; i += 4) {
-//			for(int j = 0; j < 4; j++) {
-//				TODO use Math.fma to speed this up if needed
-//				destination[i + j] = 
-//				     a[i]     * b[j]
-//				   + a[i + 1] * b[j + 4]
-//				   + a[i + 2] * b[j + 8]
-//				   + a[i + 3] * b[j + 12];
-//			}
-//		}
-//	}
-	
 	private int register(ModelData[] models) {
 		//count totals
 		int indexTotal = 0;
@@ -253,8 +185,6 @@ public final class ModelRenderProgram extends ShaderProgram {
 		glEnableVertexAttribArray(attribute_textureCoord);
 		glVertexAttribPointer(attribute_textureCoord, TEXTURE_COORD_SIZE, GL_FLOAT, false, 0, 0);
 		
-		//setupUBO();
-		
 		int indicesID = glGenBuffers();
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indicesID);
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexData, GL_STATIC_DRAW);
@@ -265,101 +195,15 @@ public final class ModelRenderProgram extends ShaderProgram {
 		return vao;
 	}
 	
-//	private int 
-//		instanceDataID,
-//		instanceDataSize, 
-//		transformOffset, 
-//		opacityOffset, 
-//		transformMatrixStride,
-//		transformArrayStride,
-//		opacityArrayStride;
-	
-//	private void loadInstanceData(ByteBuffer dest, float posX, float posY, 
-//			float scaleX, float scaleY, float rotation, float opacity) {
-//		int start = dest.position();
-//		int transformStart = start + transformOffset;
-//		float rotX = (float)Math.cos(rotation);
-//		float rotY = (float)Math.sin(rotation);
-//		//column major
-//		dest.position(transformStart)
-//			.putFloat(scaleX * rotX)
-//			.putFloat(scaleX * -rotY)
-//			.putFloat(0)
-//			.putFloat(0)
-//			.putFloat(scaleY * rotY)
-//			.putFloat(scaleY * rotX)
-//			.putFloat(0)
-//			.putFloat(0)
-//			.putFloat(0)
-//			.putFloat(0)
-//			.putFloat(0)
-//			.putFloat(0)
-//			.putFloat(posX)
-//			.putFloat(posY)
-//			.putFloat(0)
-//			.putFloat(1)
-//			.putFloat(opacity);
-//	}
-	
 	private static void prepInstanceData(FloatBuffer dest, float posX, float posY, 
 			float scaleX, float scaleY, float rotation) {
 		float rotX = (float)Math.cos(rotation);
 		float rotY = (float)Math.sin(rotation);
-		//column major
-		dest.put(scaleX * rotX)
-			.put(scaleX * -rotY)
-			.put(0)
-			.put(0)
-			.put(scaleY * rotY)
-			.put(scaleY * rotX)
-			.put(0)
-			.put(0)
-			.put(0)
-			.put(0)
-			.put(0)
-			.put(0)
-			.put(posX)
-			.put(posY)
-			.put(0)
-			.put(1);
+		dest.put(scaleX * rotX)	.put(scaleX * -rotY)	.put(0).put(0) //column major
+			.put(scaleY * rotY)	.put(scaleY * rotX)		.put(0).put(0)
+			.put(0)				.put(0)					.put(0).put(0)
+			.put(posX)			.put(posY)				.put(0).put(1);
 	}
-	
-//	private void loadTransformationMatrix(float posX, float posY, float scaleX, float scaleY, float rotation) {
-//		float rotX = (float)Math.cos(rotation);
-//		float rotY = (float)Math.sin(rotation);
-//		transformationMatrix[SCALE_X] = scaleX * rotX;
-//		transformationMatrix[SHEAR_X] = scaleY * rotY;
-//		transformationMatrix[TRANS_X] = posX;
-//		transformationMatrix[SHEAR_Y] = scaleX * -rotY;
-//		transformationMatrix[SCALE_Y] = scaleY * rotX;
-//		transformationMatrix[TRANS_Y] = posY;
-//	}
-	
-	//can use fixed size array with ubo or variable size SSO using extension to enable in opengl 4.1
-//	private void setupUBO() {
-//		int transformIndex = glGetUniformIndices(programID, "transform");
-//		int opacityIndex = glGetUniformIndices(programID, "opacity");
-//		int instanceDataIndex = glGetUniformBlockIndex(programID, "InstanceData");
-//		
-//		//TODO use glGetActiveUniformBlockiv to get all of these at once.
-//		instanceDataSize = glGetActiveUniformBlocki(programID, instanceDataIndex, GL_UNIFORM_BLOCK_DATA_SIZE);
-//		transformOffset = glGetActiveUniformsi(programID, transformIndex, GL_UNIFORM_OFFSET);
-//		opacityOffset = glGetActiveUniformsi(programID, opacityIndex, GL_UNIFORM_OFFSET);
-//		transformMatrixStride = glGetActiveUniformsi(programID, transformIndex, GL_UNIFORM_MATRIX_STRIDE);
-//		transformArrayStride = glGetActiveUniformsi(programID, transformIndex, GL_UNIFORM_ARRAY_STRIDE);
-//		opacityArrayStride = glGetActiveUniformsi(programID, opacityIndex, GL_UNIFORM_ARRAY_STRIDE);
-//		
-//		instanceDataID = glGenBuffers();
-//		glBindBuffer(GL_UNIFORM_BUFFER, instanceDataID);
-//		glBindBufferBase(GL_UNIFORM_BUFFER, instanceDataIndex, instanceDataID);
-//		glBufferData(GL_UNIFORM_BUFFER, instanceDataSize, GL_STREAM_DRAW);
-//
-//		System.out.println("size: " + instanceDataSize + 
-//				" transform " + transformIndex + " opacity " + opacityIndex + 
-//				", transform offset: " + transformOffset + 
-//				", opacity offset: " + opacityOffset + 
-//				" transform matrix stride " + transformMatrixStride);
-//	}
 	
 	public void queueRender(int modelID, float opacity,
 				float posX, float posY, float scaleX, float scaleY,
@@ -368,17 +212,16 @@ public final class ModelRenderProgram extends ShaderProgram {
 	}
 	
 	//TODO maybe I should change the queue to actually buffer indices, etc. directly instead of RenderInstances
-	public void queueRender(RenderInstance model) {
+	public void renderQueue(RenderInstance model) {
 		renderQueue.add(Objects.requireNonNull(model));
 	}
 	
-	public void queueRender(Collection<RenderInstance> models) {
+	public void renderQueue(Collection<RenderInstance> models) {
 		renderQueue.addAll(models);
 	}
 	
 	private float lastOpacity = 0;
 	
-	//TODO something is causing a crash
 	/** Render queued RenderInstances **/
 	public void render() {
 		//glMultiDrawElementsIndirect works even better but requires OpenGL 4.3
@@ -400,43 +243,6 @@ public final class ModelRenderProgram extends ShaderProgram {
 				glDrawElements(GL_TRIANGLES, model.indexCount, GL_UNSIGNED_INT, model.indexOffset * Integer.BYTES);	
 			}
 		}
-		
-		/*
-		try(MemoryStack stack = MemoryStack.stackPush()) {
-			if(stack.getPointer() < renderQueue.size() * Integer.BYTES * Pointer.POINTER_SIZE * instanceDataSize) {
-				throw new RuntimeException("not enough space to allocate multidraw buffers on stack: "
-						+ Utility.formatSize(stack.getPointer()));	
-			}
-			
-			IntBuffer counts = stack.mallocInt(renderQueue.size());
-			PointerBuffer offsets = stack.mallocPointer(renderQueue.size());
-			
-			//size of a single uniform times mdoels.size TODO do I need to allocate with alignment?
-			ByteBuffer instanceData = stack.malloc(instanceDataSize * renderQueue.size());
-			
-			while(!renderQueue.isEmpty()) {
-				RenderInstance model = renderQueue.poll();
-				ModelAttributes data = modelProperties.get(model.modelID);
-				offsets.put(data.indexOffset * Integer.BYTES);
-				counts.put(data.indexCount);
-				
-				//temporary
-				//setFloat(uniform_opacity, model.opacity);
-				//loadTransformationMatrix(model.posX, model.posY, model.scaleX, model.scaleY, model.rotation);	
-				//setMatrix(uniform_transform, transformationMatrix);
-				
-				//TODO put transform matrices into in vbo/uniform aligned based on earlier queried offsets/alignment
-				loadInstanceData(instanceData.position(), model.posX, model.posY, 
-						model.scaleX, model.scaleY, model.rotation, model.opacity);
-				
-			}
-			glBindBuffer(GL_UNIFORM_BUFFER, instanceDataID);
-			glBufferData(GL_UNIFORM_BUFFER, instanceData.flip(), GL_STREAM_DRAW);
-			glBindVertexArray(vaoID);
-			glMultiDrawElements(GL_TRIANGLES, counts.flip(), GL_UNSIGNED_INT, offsets.flip());
-		}
-		GraphicsUtility.checkErrors();
-		*/
 	}
 	
 	public static final class RenderInstance {
