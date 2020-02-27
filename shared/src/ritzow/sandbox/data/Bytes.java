4b07524d0dcfddd29cec8e04e4f8ab98e4089068
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2020-02-27 12:01:48 -0800

    Updated compression/decompression

--- shared/src/ritzow/sandbox/data/Bytes.java
+++ shared/src/ritzow/sandbox/data/Bytes.java
@@ -1,13 +1,10 @@
 package ritzow.sandbox.data;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.zip.DeflaterOutputStream;
-import java.util.zip.InflaterOutputStream;
+import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 
@@ -17,12 +14,7 @@ import java.util.zip.Inflater;
  */
 public final class Bytes {
 	public static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
-	
-	public static void checkCapacity(byte[] dest, int offset, int count) {
-		if(count > dest.length - offset)
-			throw new IllegalStateException("capacity exceeded");
-	}
-	
+
 	/**
 	 * Splits source into arrays of at most {@code length} bytes.
 	 * @param source the source array.
@@ -44,7 +36,7 @@ public final class Bytes {
 		}
 		return dest;
 	}
-	
+
 	public static <T extends Transportable> byte[] serializeCollection(Collection<T> elements, Serializer ser) {
 		byte[][] serialized = new byte[elements.size()][]; //store each serialized object's data
 		int bytes = 0; //for counting total number of bytes
@@ -55,16 +47,16 @@ public final class Bytes {
 			bytes += s.length;
 			index++;
 		}
-		
+
 		byte[] finalized = concatenate(4, bytes, serialized);
 		putInteger(finalized, 0, index);
 		return finalized;
 	}
-	
+
 	public static byte[] serializeArray(Transportable[] elements, Serializer ser) {
 		return serializeCollection(Arrays.asList(elements), ser);
 	}
-	
+
 	/**
 	 * Places a set of byte arrays into a destination array consecutively starting at the given offset.
 	 * WARNING: This method has very few safety checks
@@ -80,16 +72,15 @@ public final class Bytes {
 		int index = offset;
 		for(byte[] array : arrays) {
 			System.arraycopy(array, headerSize, destination, index, array.length - headerSize);
-			//copy(array, destination, index);
 			index += array.length;
 		}
 		return index - offset;
 	}
-	
+
 	public static int concatenate(byte[] destination, int offset, byte[]... arrays) {
 		return concatenate(destination, offset, 0, arrays);
 	}
-	
+
 	/**
 	 * Creates a byte array containing the supplied arrays starting at a given offset into the output array
 	 * @param offset the starting index of the array data
@@ -98,21 +89,27 @@ public final class Bytes {
 	 */
 	public static byte[] concatenate(int offset, byte[]... arrays) {
 		int length = offset;
-		for(byte[] array : arrays) 
+		for(byte[] array : arrays)
 			length += array.length;
 		return concatenate(offset, length, arrays);
 	}
-	
+
 	private static byte[] concatenate(int offset, int totalBytes, byte[]... arrays) {
 		byte[] dest = new byte[offset + totalBytes];
 		concatenate(dest, offset, arrays);
 		return dest;
 	}
-	
+
 	public static byte[] concatenate(byte[]... arrays) {
 		return concatenate(0, arrays);
 	}
-	
+
+	public static byte[] expand(byte[] source, int extraLength) {
+		byte[] copy = new byte[source.length + extraLength];
+		System.arraycopy(source, 0, copy, 0, source.length);
+		return copy;
+	}
+
 	/**
 	 * @param source the array to copy
 	 * @param destination the destination array
@@ -121,27 +118,30 @@ public final class Bytes {
 	public static void copy(byte[] source, byte[] destination, int offset) {
 		System.arraycopy(source, 0, destination, offset, source.length);
 	}
-	
+
 	public static byte[] subArray(byte[] array, int offset, int length) {
 		byte[] sub = new byte[length];
 		System.arraycopy(array, offset, sub, 0, length);
 		return sub;
 	}
-	
+
 	/* bytes to values */
-	
+
 	public static double getDouble(byte[] array, int index) {
 		return Double.longBitsToDouble(Bytes.getLong(array, index));
 	}
-	
+
 	public static float getFloat(byte[] array, int index) {
 		return Float.intBitsToFloat(getInteger(array, index));
 	}
-	
+
 	public static int getInteger(byte[] array, int index) {
-		return ((array[index] & 255) << 24) | ((array[index + 1] & 255) << 16) | ((array[index + 2] & 255) << 8) | ((array[index + 3] & 255) << 0);
+		return ((array[index] & 255) << 24) |
+				((array[index + 1] & 255) << 16) |
+				((array[index + 2] & 255) << 8) |
+				((array[index + 3] & 255));
 	}
-	
+
 	public static long getLong(byte[] array, int index) {
 		long value = 0;
 		for (int i = 0; i < 8; i++) {
@@ -149,133 +149,164 @@ public final class Bytes {
 		}
 		return value;
 	}
-	
+
 	public static short getShort(byte[] array, int index) {
 		return (short)((array[index] << 8) | (array[index + 1] & 255));
 	}
-	
+
 	public static boolean getBoolean(byte[] array, int index) {
-		return array[index] == 1 ? true : false;
+		return array[index] == 1;
 	}
-	
+
 	public static String getString(byte[] array, int index, Charset charset) {
 		return new String(array, index + 4, getInteger(array, index), charset);
 	}
-	
+
 	public static int getStringLength(byte[] array, int index) {
 		return getInteger(array, index) + 4;
 	}
-	
+
 	/* values to bytes */
-	
+
 	public static void putDouble(byte[] array, int index, double value) {
 		putLong(array, index, Double.doubleToRawLongBits(value));
 	}
-	
+
 	public static void putFloat(byte[] array, int index, float value) {
 		putInteger(array, index, Float.floatToRawIntBits(value));
 	}
-	
+
 	public static void putInteger(byte[] array, int index, int value) {
 		array[index + 0] = (byte)(value >>> 24);
 		array[index + 1] = (byte)(value >>> 16);
 		array[index + 2] = (byte)(value >>> 8);
 		array[index + 3] = (byte)(value >>> 0);
 	}
-	
+
 	public static void putLong(byte[] array, int index, long value) {
 	    for (int i = 7; i >= 0; i--) {
 	        array[index + i] = (byte)(value & 255);
 	        value >>= 8;
 	    }
 	}
-	
+
 	public static void putShort(byte[] array, int index, short value) {
 		array[index + 0] = (byte)((value >>> 8) & 0xFF);
 		array[index + 1] = (byte)((value >>> 0) & 0xFF);
 	}
-	
+
 	public static void putBoolean(byte[] array, int index, boolean b) {
 		array[index] = (byte)(b ? 1 : 0);
 	}
-	
+
 	public static int putBytesWithLength(byte[] array, int index, byte[] bytes) {
 		putInteger(array, index, bytes.length);
 		copy(bytes, array, index + 4);
 		return bytes.length + 4;
 	}
-	
+
 	public static int putString(byte[] array, int index, String str, Charset charset) {
 		return putBytesWithLength(array, index, str.getBytes(charset));
 	}
-	
+
 	public static byte[] get(ByteBuffer in, int length) {
 		byte[] data = new byte[length];
 		in.get(data);
 		return data;
 	}
-	
+
 	public static String getString(ByteBuffer in, int length, Charset charset) {
 		return new String(get(in, length), charset);
 	}
-	
+
+	public static byte[] compress(byte[] data) {
+		return compress(data, false);
+	}
+
 	//TODO add custom optimized huffman-coding compression, especially regarding World/Entity data.
 	/**
 	 * Compressed byte data using the deflate algorithm provided by java
 	 * @param data the byte array to compress
 	 * @return a new compressed byte array
 	 */
-	public static byte[] compress(byte[] data) {
-		Deflater deflate = new Deflater(2, false);
+	public static byte[] compress(byte[] data, boolean isRandom) {
+		Deflater deflate = new Deflater(Deflater.BEST_COMPRESSION, true);
+		deflate.setStrategy(isRandom ? Deflater.FILTERED : Deflater.DEFAULT_STRATEGY);
+		deflate.setInput(data);
+		deflate.finish();
+		byte[] out = new byte[data.length];
+		int length = deflate.deflate(out);
+		while(!deflate.finished()) {
+			//in case the compressed data ends up being larger than the input
+			out = Bytes.expand(out, length/2);
+			length += deflate.deflate(out, length, length/2); //add new data
+		}
+		deflate.end();
+		return Bytes.subArray(out, 0, length);
+	}
+
+	public static ByteBuffer decompress(ByteBuffer data) {
 		try {
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			try(var deflater = new DeflaterOutputStream(new DeflaterOutputStream(out))) {
-				deflater.write(data);
-				return out.toByteArray();	
+			Inflater inflater = new Inflater(true);
+			inflater.setInput(data);
+			ByteBuffer out = ByteBuffer.allocate(data.remaining() * 2); //assume 50% compression
+			inflater.inflate(out);
+			while(!inflater.finished()) { //inflater.getRemaining() > 0
+				System.out.println("not finished, continue decompression");
+				inflater.inflate(out = ByteBuffer.allocate(out.flip().remaining() * 2).put(out));
 			}
-		} catch(IOException e) {
+			inflater.end();
+			return out.flip();
+		} catch (DataFormatException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	public static byte[] decompress(byte[] data, int offset, int length) {
-		ByteArrayOutputStream out = new ByteArrayOutputStream(length);
-		try(var inflater = new InflaterOutputStream(new InflaterOutputStream(out))) {
-			inflater.write(data, offset, length);
-			return out.toByteArray();
-		} catch (IOException e) {
+		try {
+			byte[] out = new byte[data.length + 50]; //assume 50% compression
+			Inflater inflater = new Inflater(true);
+			inflater.setInput(data, offset, length);
+			int pos = inflater.inflate(out);
+
+			while(!inflater.finished()) { //inflater.getRemaining() > 0
+				System.out.println("not finished, continue decompression");
+				byte[] next = new byte[out.length * 2];
+				Bytes.copy(out, next, pos);
+				pos += inflater.inflate(next);
+				out = next;
+			}
+			inflater.end();
+			return Bytes.subArray(out, 0, pos);
+		} catch(DataFormatException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	public static byte[] decompress(byte[] data) {
-		return decompress(data, 0, data.length);
-	}
-	
+
 	public static byte[] of(short value) {
 		byte[] packet = new byte[2];
 		Bytes.putShort(packet, 0, value);
 		return packet;
 	}
-	
+
 	public static byte[] of(int value) {
 		byte[] packet = new byte[4];
 		Bytes.putInteger(packet, 0, value);
 		return packet;
 	}
-	
+
 	public static byte[] of(long value) {
 		byte[] packet = new byte[8];
 		Bytes.putLong(packet, 0, value);
 		return packet;
-	}	
-	
+	}
+
 	public static byte[] of(float value) {
 		byte[] packet = new byte[4];
 		Bytes.putFloat(packet, 0, value);
 		return packet;
 	}
-	
+
 	public static byte[] of(double value) {
 		byte[] packet = new byte[8];
 		Bytes.putDouble(packet, 0, value);
