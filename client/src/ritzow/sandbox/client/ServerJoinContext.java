13099b432730f79c7d0b3c43b3a1c15584ee842c
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2020-02-29 18:19:05 -0800

    Updated logging

--- client/src/ritzow/sandbox/client/ServerJoinContext.java
+++ client/src/ritzow/sandbox/client/ServerJoinContext.java
@@ -5,62 +5,62 @@ import java.net.BindException;
 import java.net.PortUnreachableException;
 import java.nio.ByteBuffer;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import ritzow.sandbox.client.input.InputContext;
 import ritzow.sandbox.client.network.Client;
 import ritzow.sandbox.network.NetworkUtility;
 import ritzow.sandbox.network.Protocol;
 
-import static ritzow.sandbox.client.data.StandardClientOptions.*;
+import static ritzow.sandbox.client.data.StandardClientOptions.LAST_LOCAL_ADDRESS;
+import static ritzow.sandbox.client.data.StandardClientOptions.LAST_SERVER_ADDRESS;
+import static ritzow.sandbox.client.util.ClientUtility.log;
 
 class ServerJoinContext {
 	private final GameState state;
 	private final Client client;
-	
-	private static final Logger SERVER_JOIN_LOG = Logger.getLogger("server_join");
-	
+
 	public static void start(GameState state) throws IOException {
 		try {
-		SERVER_JOIN_LOG.info("Connecting to " + NetworkUtility.formatAddress(LAST_SERVER_ADDRESS)
-			+ " from " + NetworkUtility.formatAddress(LAST_LOCAL_ADDRESS) + "... ");
+		log().info("Connecting to " + NetworkUtility.formatAddress(LAST_SERVER_ADDRESS)
+			+ " from " + NetworkUtility.formatAddress(LAST_LOCAL_ADDRESS));
 			Client client = Client.create(LAST_LOCAL_ADDRESS, LAST_SERVER_ADDRESS);
 			ServerJoinContext context = new ServerJoinContext(state, client);
 			client.setOnTimeout(context::onTimeout).setOnException(context::onException).beginConnect();
 			GameLoop.setContext(context::listen);
 		} catch(BindException e) {
-			SERVER_JOIN_LOG.warning("bind error: " + e.getMessage() + ".");
-			GameLoop.setContext(state.menuContext::update);	
+			log().log(Level.WARNING, "Bind error", e);
+				//.warning("Bind error: " + e.getMessage() + ".", );
+			GameLoop.setContext(state.menuContext::update);
 		}
 	}
-	
+
 	private ServerJoinContext(GameState state, Client client) {
 		this.state = state;
 		this.client = client;
 	}
-	
+
 	private void listen() {
 		state.display.poll(InputContext.EMPTY_CONTEXT);
 		client.update(1, this::process);
 	}
-	
+
 	private void onTimeout() {
-		SERVER_JOIN_LOG.warning("timed out.");
+		log().warning("Connection timed out.");
 		returnToMenu();
 	}
 
 	private void onException(IOException e) {
 		if(e instanceof PortUnreachableException) {
-			SERVER_JOIN_LOG.info("server port unreachable.");
+			log().warning("Server port unreachable.");
 		} else {
-			SERVER_JOIN_LOG.log(Level.SEVERE, "An IOException occurred while joining server", e);
+			log().log(Level.SEVERE, "An IOException occurred while joining server", e);
 		}
 		returnToMenu();
 	}
-	
+
 	private void returnToMenu() {
 		try {
 			client.close();
-			GameLoop.setContext(state.menuContext::update);	
+			GameLoop.setContext(state.menuContext::update);
 		} catch(IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -71,13 +71,13 @@ class ServerJoinContext {
 			byte response = data.get();
 			switch(response) {
 				case Protocol.CONNECT_STATUS_REJECTED -> {
-					SERVER_JOIN_LOG.info("rejected.");
+					log().info("Server rejected connection");
 					state.display.resetCursor();
 					returnToMenu();
 				}
 
 				case Protocol.CONNECT_STATUS_WORLD -> {
-					SERVER_JOIN_LOG.info("connected.\nReceiving world data... ");
+					log().info("Connected to server");
 					//worldSize and playerID integers
 					InWorldContext worldContext = new InWorldContext(state, client, data.getInt(), data.getInt());
 					GameLoop.setContext(worldContext::listenForServer);
@@ -88,9 +88,6 @@ class ServerJoinContext {
 
 				default -> throw new UnsupportedOperationException("unknown connect ack type " + response);
 			}
-		} else { //TODO process will be called multiple times in the same update call, this will happen
-			throw new IllegalStateException("Received other message before SERVER_CONNECT_ACKNOWLEDGMENT: " 
-					+ messageType);
 		}
 	}
 }
--- client/src/ritzow/sandbox/client/StartClient.java
+++ client/src/ritzow/sandbox/client/StartClient.java
@@ -1,43 +1,38 @@
 package ritzow.sandbox.client;
 
-import static org.lwjgl.glfw.GLFW.glfwInit;
-import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
-import static org.lwjgl.glfw.GLFW.glfwTerminate;
-import static org.lwjgl.glfw.GLFW.glfwDestroyCursor;
-import static ritzow.sandbox.client.data.StandardClientProperties.*;
-
 import java.io.IOException;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import ritzow.sandbox.client.audio.AudioSystem;
 import ritzow.sandbox.client.data.StandardClientOptions;
-import ritzow.sandbox.client.graphics.*;
+import ritzow.sandbox.client.graphics.Display;
+import ritzow.sandbox.client.graphics.RenderManager;
 import ritzow.sandbox.client.util.ClientUtility;
 import ritzow.sandbox.util.Utility;
 
+import static org.lwjgl.glfw.GLFW.*;
+import static ritzow.sandbox.client.data.StandardClientProperties.CURSORS_PATH;
+import static ritzow.sandbox.client.data.StandardClientProperties.TEXTURES_PATH;
+import static ritzow.sandbox.client.util.ClientUtility.log;
+
 /** Entry point to Sandbox2D game client **/
 class StartClient {
-	
+
 	/** Native launcher entry point.
 	 * @param args command line arguments as a single String.
-	 * @throws Exception if an exception occurrs during program execution **/
+	 * @throws Exception if an exception occurs during program execution **/
 	public static void start(String args) throws Exception {
 		start();
 	}
-	
+
 	/** Command line entry point.
 	 * @param args command line arguments.
 	 * @throws Exception if the program encounters an error. **/
 	public static void main(String[] args) throws Exception {
 		start();
 	}
-	
+
 	private static void start() throws Exception {
-		setupLogging();
-		Logger.getGlobal().info("Starting game... ");
+		log().info("Starting game");
 		long startupStart = System.nanoTime();
 		AudioSystem audio = AudioSystem.getDefault(); //load default audio system
 		GameState state = new GameState();
@@ -45,12 +40,12 @@ class StartClient {
 		state.display.setGraphicsContextOnThread();
 		state.shader = RenderManager.setup();
 		MainMenuContext mainMenu = new MainMenuContext(state);
-		Logger.getGlobal().info("took " + Utility.formatTime(Utility.nanosSince(startupStart)));
+		log().info("Game startup took " + Utility.formatTime(Utility.nanosSince(startupStart)));
 		state.display.show();
 		try {
 			GameLoop.start(mainMenu::update);
 		} finally {
-			Logger.getGlobal().info("Exiting... ");
+			log().info("Exiting game");
 			state.shader.delete();
 			RenderManager.closeContext();
 			state.display.destroy();
@@ -58,12 +53,13 @@ class StartClient {
 			glfwDestroyCursor(state.cursorPick);
 			glfwDestroyCursor(state.cursorMallet);
 			glfwTerminate();
-			Logger.getGlobal().info("done!");	
+			log().info("Game exited");
 		}
 	}
 
 	private static void setupGLFW(GameState state) throws IOException {
-		glfwSetErrorCallback(GLFWErrorCallback.createPrint(System.err));
+		log().info("Loading GLFW and creating window");
+		glfwSetErrorCallback(GLFWErrorCallback.createThrow());
 		if(!glfwInit()) throw new RuntimeException("GLFW failed to initialize");
 		var appIcon   = ClientUtility.loadGLFWImage(TEXTURES_PATH.resolve("redSquare.png"));
 		var cursorPickaxe = ClientUtility.loadGLFWImage(CURSORS_PATH.resolve("pickaxe32.png"));
@@ -72,5 +68,4 @@ class StartClient {
 		state.cursorMallet = ClientUtility.loadGLFWCursor(cursorMallet, 0, 0.66f);
 		state.display = new Display(4, StandardClientOptions.USE_OPENGL_4_6 ? 6 : 1, "Sandbox2D", appIcon);
 	}
-	
 }
