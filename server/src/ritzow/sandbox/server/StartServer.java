eec36d3ff8bca8cdd2ad23d3a61f84a51a5ff19f
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2020-02-29 18:19:46 -0800

    Updated StartServer world saving

--- server/src/ritzow/sandbox/server/StartServer.java
+++ server/src/ritzow/sandbox/server/StartServer.java
@@ -3,8 +3,10 @@ package ritzow.sandbox.server;
 import java.io.IOException;
 import java.net.BindException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import ritzow.sandbox.data.Bytes;
 import ritzow.sandbox.network.NetworkUtility;
 import ritzow.sandbox.network.Protocol;
@@ -15,20 +17,25 @@ import ritzow.sandbox.world.World;
 import ritzow.sandbox.world.block.DirtBlock;
 import ritzow.sandbox.world.block.GrassBlock;
 import ritzow.sandbox.world.entity.ItemEntity;
-import ritzow.sandbox.world.entity.PlayerEntity;
 
 @SuppressWarnings("unused")
 public final class StartServer {
-	private static final Path SAVE_FILE = Path.of("world.dat");
-	private static final int WORLD_WIDTH = 150, WORLD_BASE_HEIGHT = 15, TERRAIN_AMPLITUDE = 10, SKY_HEIGHT = 20;
-	private static final float WORLD_FREQUENCY = 0.05f;
-	
+	private static final Path
+			SAVE_FILE = Path.of("world.dat");
+	private static final int
+			WORLD_WIDTH = 1500,
+			WORLD_BASE_HEIGHT = 150,
+			TERRAIN_AMPLITUDE = 10,
+			SKY_HEIGHT = 20;
+	private static final float
+			WORLD_FREQUENCY = 0.05f;
+
 	private static Server server;
 	private static boolean save = true;
 
 	public static void main(String[] args) throws IOException {
-		InetSocketAddress bind = args.length > 0 ? 
-			NetworkUtility.parseSocket(args[0], Protocol.DEFAULT_SERVER_PORT) : 
+		InetSocketAddress bind = args.length > 0 ?
+			NetworkUtility.parseSocket(args[0], Protocol.DEFAULT_SERVER_PORT) :
 			new InetSocketAddress(NetworkUtility.getPrimaryAddress(true), Protocol.DEFAULT_SERVER_PORT);
 		try {
 			startServer(bind);
@@ -41,13 +48,13 @@ public final class StartServer {
 				server.update();
 				Utility.sleepMillis(1);
 			}
-			
+
 			saveWorld(server.world(), SAVE_FILE);
 		} catch(BindException e) {
 			System.out.println("Couldn't start server on address " + NetworkUtility.formatAddress(bind));
 		}
 	}
-	
+
 	public static void startServer(InetSocketAddress bind) throws IOException {
 		server = Server.start(bind);
 		System.out.println("Started server on " + NetworkUtility.formatAddress(server.getAddress()) + ".");
@@ -59,7 +66,13 @@ public final class StartServer {
 	}
 
 	public static World loadWorld(Path file) throws IOException {
-		return SerializationProvider.getProvider().deserialize(Bytes.decompress(Files.readAllBytes(file)));
+		try(var in = Files.newByteChannel(file, StandardOpenOption.READ)) {
+			if(in.size() > Integer.MAX_VALUE)
+				throw new RuntimeException("file too large to read");
+			ByteBuffer buffer = ByteBuffer.allocate((int)in.size());
+			in.read(buffer);
+			return SerializationProvider.getProvider().deserialize(Bytes.decompress(buffer.flip()));
+		}
 	}
 
 	private static World generateWorld() {
@@ -71,7 +84,7 @@ public final class StartServer {
 		fg.fill(DirtBlock.INSTANCE, 0, 0, fg.getWidth(), WORLD_BASE_HEIGHT);
 		bg.fill(DirtBlock.INSTANCE, 0, 0, bg.getWidth(), WORLD_BASE_HEIGHT);
 		for(int column = 0; column < WORLD_WIDTH; ++column) {
-			int max = base + midpoint + Math.round(midpoint * (float)Math.sin(column * WORLD_FREQUENCY));	
+			int max = base + midpoint + Math.round(midpoint * (float)Math.sin(column * WORLD_FREQUENCY));
 			for(int row = base; row <= max; ++row) {
 				fg.set(column, row, DirtBlock.INSTANCE);
 				bg.set(column, row, DirtBlock.INSTANCE);
@@ -86,12 +99,13 @@ public final class StartServer {
 		if(save) {
 			try {
 				System.out.print("Saving world... ");
-				world.removeIf(e -> e instanceof PlayerEntity); //remove players before saving to file
+				//shouldnt have any players once server is shut down
+				//world.removeIf(e -> e instanceof PlayerEntity); //remove players before saving to file
 				byte[] serialized = Bytes.compress(SerializationProvider.getProvider().serialize(world));
 				Files.write(file, serialized);
 				System.out.println("world saved to " + Utility.formatSize(serialized.length) + ".");
 			} catch(IOException e) {
-				System.out.println("Error while saving world to file '" + file + "':" 
+				System.out.println("Error while saving world to file '" + file + "':"
 					+ e.getClass().getTypeName() + ":" + e.getMessage());
 			}
 		} else {
@@ -110,17 +124,17 @@ public final class StartServer {
 				.register("printworld", StartServer::printworldCommand, false)
 				.register("killitems", 	StartServer::killItemsCommand, false);
 	}
-	
+
 	private static void killItemsCommand(String args) {
 		int entities = server.world().entities();
 		server.world().removeIf(e -> e instanceof ItemEntity);
 		System.out.println("Removed " + (entities - server.world().entities()) + " items from the world.");
 	}
-	
+
 	private static void printworldCommand(String args) {
 		System.out.println(server.world());
 	}
-	
+
 	private static void debugCommand(String args) {
 		server.printDebug(System.out);
 	}
@@ -139,12 +153,12 @@ public final class StartServer {
 	private static void stopCommand(String args) {
 		server.startShutdown();
 	}
-	
+
 	private static void abortCommand(String args) {
 		save = false;
 		server.startShutdown();
 	}
-	
+
 	private static void killCommand(String args) {
 		save = false;
 		try {
