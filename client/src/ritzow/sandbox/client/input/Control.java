4855f7ca220608bedfd43a7cadb112f5772a6968
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2020-03-15 15:12:24 -0700

    Refactored Control

--- client/src/ritzow/sandbox/client/input/Control.java
+++ client/src/ritzow/sandbox/client/input/Control.java
@@ -2,31 +2,33 @@ package ritzow.sandbox.client.input;
 
 import static org.lwjgl.glfw.GLFW.*;
 
-public class Control {
-	public static final Button QUIT = 			new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_ESCAPE);
-	public static final Button FULLSCREEN =		new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_F11);
-	public static final Button CONNECT =		new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_C);	
-	public static final Button ACTIVATE_UI_ELEMENT = 	new Button(Button.TYPE_MOUSE, GLFW_MOUSE_BUTTON_LEFT);
+public interface Control {
+	Button QUIT = 			new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_ESCAPE);
+	Button FULLSCREEN =		new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_F11);
+	Button CONNECT =		new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_C);
+	Button ACTIVATE_UI_ELEMENT = 	new Button(Button.TYPE_MOUSE, GLFW_MOUSE_BUTTON_LEFT);
 
-	public static final Button INCREASEZOOM = 	new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_EQUAL);
-	public static final Button DECREASEZOOM = 	new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_MINUS);
-	public static final Button RESET_ZOOM = 	new Button(Button.TYPE_MOUSE, GLFW_MOUSE_BUTTON_MIDDLE);
-	
-	public static final Button SCROLL_MODIFIER = new Button(Button.TYPE_KEYBOARD, GLFW_KEY_RIGHT_SHIFT);
-	public static final Button USE_HELD_ITEM = 	new Button(Button.TYPE_MOUSE, GLFW_MOUSE_BUTTON_LEFT);
-	public static final Button THROW_BOMB = 	new Button(Button.TYPE_KEYBOARD, GLFW_KEY_ENTER);
-	
-	public static final Button MOVE_UP = 			new Button(Button.TYPE_KEYBOARD, GLFW_KEY_UP);
-	public static final Button MOVE_DOWN = 			new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_DOWN);
-	public static final Button MOVE_LEFT = 			new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_LEFT);
-	public static final Button MOVE_RIGHT = 	 		new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_RIGHT);
-	
-	public static final Button SLOT_SELECT_1 =		new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_1);
-	public static final Button SLOT_SELECT_2 =		new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_2);
-	public static final Button SLOT_SELECT_3 =		new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_3);
-	
+	Button INCREASEZOOM = 	new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_EQUAL);
+	Button DECREASEZOOM = 	new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_MINUS);
+	Button RESET_ZOOM = 	new Button(Button.TYPE_MOUSE, GLFW_MOUSE_BUTTON_MIDDLE);
 
-	public static class Button {
+	Button SCROLL_MODIFIER = new Button(Button.TYPE_KEYBOARD, GLFW_KEY_RIGHT_SHIFT);
+	Button USE_HELD_ITEM = 	new Button(Button.TYPE_MOUSE, GLFW_MOUSE_BUTTON_LEFT);
+	Button THROW_BOMB = 	new Button(Button.TYPE_KEYBOARD, GLFW_KEY_ENTER);
+
+	Button MOVE_UP = 			new Button(Button.TYPE_KEYBOARD, GLFW_KEY_UP);
+	Button MOVE_DOWN = 			new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_DOWN);
+	Button MOVE_LEFT = 			new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_LEFT);
+	Button MOVE_RIGHT = 	 		new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_RIGHT);
+
+	Button SLOT_SELECT_1 =		new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_1);
+	Button SLOT_SELECT_2 =		new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_2);
+	Button SLOT_SELECT_3 =		new Button(Button.TYPE_KEYBOARD,  GLFW_KEY_3);
+
+	byte type();
+	int code();
+
+	class Button implements Control {
 		public static final byte TYPE_MOUSE = 0, TYPE_KEYBOARD = 1;
 
 		public byte buttonType;
@@ -52,6 +54,16 @@ public class Control {
 					obj instanceof Button b &&
 					buttonType == b.buttonType &&
 					buttonCode == b.buttonCode);
+		}
+
+		@Override
+		public byte type() {
+			return buttonType;
+		}
+
+		@Override
+		public int code() {
+			return buttonCode;
 		}
 	}
 }
