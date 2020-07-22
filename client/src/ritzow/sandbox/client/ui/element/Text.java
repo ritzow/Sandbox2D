d9121c689300f31ad2a19011fd6d14e7e3a0578f
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2020-07-21 23:06:21 -0700

    Updated GUI Text class to use generic CharSequence with codePoints method
    Will be able to handle unicode better this way the future.

--- client/src/ritzow/sandbox/client/ui/element/Text.java
+++ client/src/ritzow/sandbox/client/ui/element/Text.java
@@ -1,11 +1,14 @@
 package ritzow.sandbox.client.ui.element;
 
+import java.util.PrimitiveIterator.OfInt;
+import ritzow.sandbox.client.graphics.GameModels;
+import ritzow.sandbox.client.graphics.Model;
 import ritzow.sandbox.client.ui.*;
 
 public class Text implements GuiElement {
 	private static final float SIZE_SCALE = 0.02f;
 
-	private final String text;
+	private final CharSequence text;
 	private final Font font;
 	private final float startPos, startSpacing, size;
 	private final Rectangle bounds;
@@ -17,20 +20,21 @@ public class Text implements GuiElement {
 	 * @param size Font size, in points
 	 * @param spacing The fraction of a character width to put between each character
 	 */
-	public Text(String text, Font font, int size, float spacing) {
+	public Text(CharSequence text, Font font, int size, float spacing) {
 		this.text = text;
 		this.font = font;
 		this.size = size * SIZE_SCALE;
 		this.startSpacing = this.size * (1 + spacing);
-		this.startPos = computeStart();
+		long length = text.codePoints().count();
+		this.startPos = computeStart(length);
 		//TODO remove left vs centered (moved to stuff like absolueguipositioner)
-		this.bounds = new Rectangle(text.length() * this.size + Math.max(0, (text.length() - 1) * (this.startSpacing - this.size)), this.size);
+		this.bounds = new Rectangle(length * this.size + Math.max(0, (length - 1) * (this.startSpacing - this.size)), this.size);
 	}
 
-	private float computeStart() {
+	private float computeStart(long length) {
 		float gap = this.startSpacing - this.size;
-		int half = text.length()/2;
-		if(text.length() % 2 == 0) { //a   b   c | d   e   f
+		int half = (int)(length/2);
+		if(length % 2 == 0) { //a   b   c | d   e   f
 			return -gap/2f - (half * this.size) - (half - 1) * gap + this.size/2;
 		} else { //a b c|c d e
 			return -(this.size + gap) * half;
@@ -42,8 +46,15 @@ public class Text implements GuiElement {
 		//TODO use text.codePoints().iterator() instead to more properly parse
 		//TODO include constructor field for whether to center text or start at left
 		float pos = startPos;
-		for(int index = 0; index < text.length(); pos += startSpacing, index++) {
-			renderer.draw(font.getModel(text.charAt(index)), 1.0f, pos, 0, size, size, 0);
+		OfInt iterator = text.codePoints().iterator();
+		while(iterator.hasNext()) {
+			Model model = font.getModel(iterator.nextInt());
+			if(model == null) {
+				renderer.draw(GameModels.MODEL_RED_SQUARE, 1.0f, pos, 0, size/2, size/2, 0);
+			} else {
+				renderer.draw(model, 1.0f, pos, 0, size, size, 0);
+			}
+			pos += startSpacing;
 		}
 	}
 
