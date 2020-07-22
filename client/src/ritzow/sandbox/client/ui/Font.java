eae2aa1190ac1e08d16750dbf5bccfb7e4eaa59a
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2020-07-22 00:50:05 -0700

    Attempted to reduce texture bleeding in Font, but failed

--- client/src/ritzow/sandbox/client/ui/Font.java
+++ client/src/ritzow/sandbox/client/ui/Font.java
@@ -15,36 +15,37 @@ public final class Font {
 
 	//TODO some font characters are not in correct ascii order, such as the space character
 
-	public static Font load(int glyphsPerRow, int glyphWidth, int glyphHeight, int paddingSize, int textureWidth, int textureHeight, Collection<ModelData> out,
-		float[] pos, int[] indices, float startX, float startY, float endX, float endY) {
-		return new Font(glyphsPerRow, glyphWidth, glyphHeight, paddingSize, textureWidth, textureHeight, out, pos, indices, startX, startY, endX, endY);
+	public static Font load(double texelWidth, int glyphsPerRow, int glyphWidth, int glyphHeight, int paddingSize,
+		int textureWidth, int textureHeight, Collection<ModelData> out, float[] pos, int[] indices, float startX, float startY, float endX, float endY) {
+		return new Font(texelWidth, glyphsPerRow, glyphWidth, glyphHeight, paddingSize, textureWidth, textureHeight, out, pos, indices, startX, startY, endX, endY);
 	}
 
 	private static class CharModel implements Model {}
 
 	private final CharModel[] glyphs;
-	private static final CharModel UNKNOWN_CHAR_MODEL = null; //TODO create an unknown char model
 
-	public Font(int glyphsPerRow, int glyphWidth, int glyphHeight, int paddingSize, int textureWidth, int textureHeight,
+	public Font(double texelWidth, int glyphsPerRow, int glyphWidth, int glyphHeight, int paddingSize, int textureWidth, int textureHeight,
 		Collection<ModelData> out, float[] pos, int[] indices, float leftX, float bottomY, float rightX, float topY) {
 		glyphs = new CharModel[256];
-		float width = (float)glyphWidth / textureWidth * (rightX - leftX);
-		float height = (float)glyphHeight / textureHeight * (topY - bottomY);
-		float paddingX = (float)paddingSize / textureWidth * (rightX - leftX);
-		float paddingY = (float)paddingSize / textureHeight * (topY - bottomY);
+		double texelWidthHalf = 0;//texelWidth/4d/textureWidth;
+		double texelHeightHalf = 0;//texelWidth/4d/textureHeight;
+		double width = (double)glyphWidth / textureWidth * (rightX - leftX);
+		double height = (double)glyphHeight / textureHeight * (topY - bottomY);
+		double paddingX = (double)paddingSize / textureWidth * (rightX - leftX);
+		double paddingY = (double)paddingSize / textureHeight * (topY - bottomY);
 		for(int i = 0; i < CHARACTER_LIST.length; i++) {
 			int row = i / glyphsPerRow;
 			int column = i % glyphsPerRow;
-			float top = topY - paddingY - (paddingY + height) * row;
-			float left = leftX + paddingX + (paddingX + width) * column;
-			float bottom = top - height;
-			float right = left + width;
+			float top = (float)(topY - paddingY - (paddingY + height) * row - texelHeightHalf);
+			float left = (float)(leftX + paddingX + (paddingX + width) * column + texelWidthHalf);
+			float bottom = (float)(top - height + texelHeightHalf);
+			float right = (float)(left + width - texelWidthHalf);
 			float[] coords = new float[] {left, top, left, bottom, right, bottom, right, top};
 			out.add(new ModelData(glyphs[CHARACTER_LIST[i]] = new CharModel(), pos, coords, indices));
 		}
 	}
 
 	public Model getModel(int character) {
-		return character < glyphs.length ? glyphs[character] : UNKNOWN_CHAR_MODEL;
+		return character < glyphs.length && character > 0 ? glyphs[character] : null;
 	}
 }
