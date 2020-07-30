d4d0b83231420906e55ce2dbd70ee34f00d6e853
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2020-07-29 21:20:14 -0700

    Fixed Font texture issue by correcting texture coordinate computation
    Accidentally applied half-pixel correction to top coord before using
    top coord to compute bottom, like wise for left and right.

--- client/src/ritzow/sandbox/client/ui/Font.java
+++ client/src/ritzow/sandbox/client/ui/Font.java
@@ -1,12 +1,16 @@
 package ritzow.sandbox.client.ui;
 
-import java.util.Collection;
+import java.util.function.Consumer;
 import ritzow.sandbox.client.graphics.Model;
 import ritzow.sandbox.client.graphics.ModelRenderProgramBase.ModelData;
+import ritzow.sandbox.client.graphics.RenderManager;
+import ritzow.sandbox.client.graphics.TextureAtlas;
+import ritzow.sandbox.client.graphics.TextureAtlas.AtlasRegion;
+import ritzow.sandbox.client.graphics.TextureData;
 
 public final class Font {
 
-	private static final char[] CHARACTER_LIST = new char[]{
+	private static final char[] CHARACTER_LIST = {
 		'!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 		':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
 		'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
@@ -16,8 +20,13 @@ public final class Font {
 	//TODO some font characters are not in correct ascii order, such as the space character
 
 	public static Font load(double texelWidth, int glyphsPerRow, int glyphWidth, int glyphHeight, int paddingSize,
-		int textureWidth, int textureHeight, Collection<ModelData> out, int[] indices, float startX, float startY, float endX, float endY) {
+		int textureWidth, int textureHeight, Consumer<ModelData> out, int[] indices, float startX, float startY, float endX, float endY) {
 		return new Font(texelWidth, glyphsPerRow, glyphWidth, glyphHeight, paddingSize, textureWidth, textureHeight, out, indices, startX, startY, endX, endY);
+	}
+
+	public static Font load(TextureData fontTexture, TextureAtlas atlas, AtlasRegion region, Consumer<ModelData> modelOut) {
+		return new Font(1.0d / atlas.width(), 25, 9, 9, 1, fontTexture.getWidth(), fontTexture.getHeight(), modelOut,
+			RenderManager.VERTEX_INDICES_RECT, region.leftX(), region.bottomY(), region.rightX(), region.topY());
 	}
 
 	private final Model[] glyphs;
@@ -31,13 +40,13 @@ public final class Font {
 		return glyphHeight;
 	}
 
-	public Font(double texelWidth, int glyphsPerRow, int glyphWidth, int glyphHeight, int paddingSize, int textureWidth, int textureHeight,
-		Collection<ModelData> out, int[] indices, float leftX, float bottomY, float rightX, float topY) {
+	public Font(double texelDimension, int glyphsPerRow, int glyphWidth, int glyphHeight, int paddingSize, int textureWidth, int textureHeight,
+		Consumer<ModelData> out, int[] indices, float leftX, float bottomY, float rightX, float topY) {
 		glyphs = new Model[256];
 		this.glyphWidth = glyphWidth;
 		this.glyphHeight = glyphHeight;
-		double texelWidthHalf = 0;//texelWidth/4d/textureWidth;
-		double texelHeightHalf = 0;//texelWidth/4d/textureHeight;
+		double texelWidthHalf = texelDimension/textureWidth/2d;
+		double texelHeightHalf = texelDimension/textureHeight/2d;
 		double width = (double)glyphWidth / textureWidth * (rightX - leftX);
 		double height = (double)glyphHeight / textureHeight * (topY - bottomY);
 		double paddingX = (double)paddingSize / textureWidth * (rightX - leftX);
@@ -45,10 +54,13 @@ public final class Font {
 		for(int i = 0; i < CHARACTER_LIST.length; i++) {
 			int row = i / glyphsPerRow;
 			int column = i % glyphsPerRow;
-			float top = (float)(topY - paddingY - (paddingY + height) * row - texelHeightHalf);
-			float left = (float)(leftX + paddingX + (paddingX + width) * column + texelWidthHalf);
-			float bottom = (float)(top - height + texelHeightHalf);
-			float right = (float)(left + width - texelWidthHalf);
+			double topInitial = topY - paddingY - (paddingY + height) * row;
+			double leftInitial = leftX + paddingX + (paddingX + width) * column;
+			float top = (float)(topInitial - texelHeightHalf);
+			float left = (float)(leftInitial + texelWidthHalf);
+			float bottom = (float)(topInitial - height + texelHeightHalf);
+			float right = (float)(leftInitial + width - texelWidthHalf);
+
 			float[] coords = new float[] {
 				left, top,
 				left, bottom,
@@ -66,7 +78,7 @@ public final class Font {
 			};
 
 			int charIndex = i;
-			out.add(new ModelData(model -> glyphs[CHARACTER_LIST[charIndex]] = model, glyphWidth, glyphHeight, positions, coords, indices));
+			out.accept(new ModelData(model -> glyphs[CHARACTER_LIST[charIndex]] = model, glyphWidth, glyphHeight, positions, coords, indices));
 		}
 	}
 
